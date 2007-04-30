2de7330d7c1744b6df5d0eb4d9d569006adf261d
becker <becker@git.lsstcorp.org>
2007-04-30 22:45:05 +0000

    added in variance calculation, should be coming along soon.  ACB.

--- tests/lsstimageproc01.cc
+++ tests/lsstimageproc01.cc
@@ -5,26 +5,38 @@
 using namespace std;
 using namespace lsst::fw;
 
-template <typename ImagePixelT, typename MaskPixelT> class setmaskPixProcFunc : public PixelProcessingFunc<ImagePixelT, MaskPixelT> 
+template <typename ImagePixelT, typename MaskPixelT> class synthesizeCfhtPixProcFunc : public PixelProcessingFunc<ImagePixelT, MaskPixelT> 
 {
 public:
    typedef PixelLocator<ImagePixelT> ImageIteratorT;
+   typedef PixelLocator<VariancePixelT> VarianceIteratorT;
    typedef PixelLocator<MaskPixelT> MaskIteratorT;
    typedef typename PixelChannelType<MaskPixelT>::type MaskChannelT;
 
-   setmaskPixProcFunc(MaskedImage<ImagePixelT, MaskPixelT>& m) : PixelProcessingFunc<ImagePixelT, MaskPixelT>(m), initCount(0) {}
+   setmaskPixProcFunc(MaskedImage<ImagePixelT, VariancePixelT, MaskPixelT>& m) : PixelProcessingFunc<ImagePixelT, VariancePixelT, MaskPixelT>(m), initCount(0) {}
 
    void init() {
-      PixelProcessingFunc<ImagePixelT, MaskPixelT>::_maskPtr->getPlaneBitMask("manual", bitSat);
+      PixelProcessingFunc<ImagePixelT, VariancePixelT, MaskPixelT>::_maskPtr->getPlaneBitMask("manual", bitSat);
+      DataProperty::DataPropertyPtrT metaDataPtr = PixelProcessingFunc<ImagePixelT, VariancePixelT, MaskPixelT>::_imagePtr->getMetaData();
+      DataProperty::DataPropertyPtrT satPtr = metaDataPtr->find(boost::regex("MAXLIN"));
+      DataProperty::DataPropertyPtrT gainPtr = metaDataPtr->find(boost::regex("GAIN"));
+      DataProperty::DataPropertyPtrT rdnoisePtr = metaDataPtr->find(boost::regex("RDNOISE"));
       maskCount = 0;
       initCount++;
    }
 
-   void operator ()(ImageIteratorT &i,MaskIteratorT &m) {
-      if (*i > goodHighPixel) {
+   void operator ()(ImageIteratorT &i, VarianceIterator &v, MaskIteratorT &m) {
+      if (*i > satValue) {
 	 *m = *m | bitSat;
 	 maskCount++;
       }
+      // synthesize variance :
+      // noise_e**2   = pixel_e + rdnoise**2
+      // pixel_e      = pixel_adu * gain
+      // noise_adu**2 = (noise_e / gain)**2
+      //              = pixel_e / gain**2 + (rdnoise/gain)**2
+      //              = pixel_adu / gain  + (rdnoise/gain)**2
+      *v = *i * igainValue + rdnoiseOverGainSquared;
    }
 
    int getCount() { return maskCount; }
@@ -33,7 +45,10 @@ private:
    MaskChannelT bitSat;
    int maskCount;
    int initCount;
-   int goodHighPixel = 20000;
+   float satValue;
+   float igainValue;   // inverse gain
+   float rdnoiseValue; 
+   float rdnoiseOverGainSquared;
 };
 
 
@@ -49,7 +64,7 @@ int main( int argc, char** argv )
    MaskedImage<ImagePixelType,MaskPixelType> cfhtScienceMaskedImage;
    cfhtScienceMaskedImage.readFits(argv[1]);
    cfhtScienceMaskedImage.getMask()->addMaskPlane("manual");
-   setMaskPixProcFunc<ImagePixelType, MaskPixelType> maskScienceFunc(cfhtScienceMaskedImage);
+   synthesizeCfhtPixProcFunc<ImagePixelType, MaskPixelType> maskScienceFunc(cfhtScienceMaskedImage);
    maskScienceFunc.init();
    cfhtScienceMaskedImage.processPixels(maskScienceFunc);
    std::cout << "Set " << maskScienceFunc.getCount() << " mask bits" << std::endl;
@@ -58,7 +73,7 @@ int main( int argc, char** argv )
    cfhtTemplateMaskedImage.readFits(argv[2]);
    cfhtTemplateMaskedImage.getMask()->addMaskPlane("manual");
    // Can I reuse maskScienceFunc?
-   setMaskPixProcFunc<ImagePixelType, MaskPixelType> maskTemplateFunc(cfhtTemplateMaskedImage);
+   synthesizeCfhtPixProcFunc<ImagePixelType, MaskPixelType> maskTemplateFunc(cfhtTemplateMaskedImage);
    maskTemplateFunc.init();
    cfhtTemplateMaskedImage.processPixels(maskTemplateFunc);
    std::cout << "Set " << maskTemplateFunc.getCount() << " mask bits" << std::endl;
