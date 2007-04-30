f3d548e316abe634cc688ee5bcd1a1fc98b9a271
becker <becker@git.lsstcorp.org>
2007-04-30 22:18:29 +0000

    0th order test code to read in 2 CFHT images.  ACB.

--- tests/lsstimageproc01.cc
+++ tests/lsstimageproc01.cc
@@ -1,11 +1,67 @@
-#include "lsst/fw/LsstBase.h"
+#include "lsst/fw/MaskedImage.h"
 #include "lsst/fw/Trace.h"
 
 #include <iostream>
 using namespace std;
 using namespace lsst::fw;
 
+template <typename ImagePixelT, typename MaskPixelT> class setmaskPixProcFunc : public PixelProcessingFunc<ImagePixelT, MaskPixelT> 
+{
+public:
+   typedef PixelLocator<ImagePixelT> ImageIteratorT;
+   typedef PixelLocator<MaskPixelT> MaskIteratorT;
+   typedef typename PixelChannelType<MaskPixelT>::type MaskChannelT;
+
+   setmaskPixProcFunc(MaskedImage<ImagePixelT, MaskPixelT>& m) : PixelProcessingFunc<ImagePixelT, MaskPixelT>(m), initCount(0) {}
+
+   void init() {
+      PixelProcessingFunc<ImagePixelT, MaskPixelT>::_maskPtr->getPlaneBitMask("manual", bitSat);
+      maskCount = 0;
+      initCount++;
+   }
+
+   void operator ()(ImageIteratorT &i,MaskIteratorT &m) {
+      if (*i > goodHighPixel) {
+	 *m = *m | bitSat;
+	 maskCount++;
+      }
+   }
+
+   int getCount() { return maskCount; }
+
+private:
+   MaskChannelT bitSat;
+   int maskCount;
+   int initCount;
+   int goodHighPixel = 20000;
+};
+
+
+
 int main( int argc, char** argv )
 {
-    return 1;
+   Trace::setDestination(std::cout);
+   Trace::setVerbosity(".", 0);
+
+   typedef uint8 MaskPixelType;
+   typedef float32 ImagePixelType;
+
+   MaskedImage<ImagePixelType,MaskPixelType> cfhtScienceMaskedImage;
+   cfhtScienceMaskedImage.readFits(argv[1]);
+   cfhtScienceMaskedImage.getMask()->addMaskPlane("manual");
+   setMaskPixProcFunc<ImagePixelType, MaskPixelType> maskScienceFunc(cfhtScienceMaskedImage);
+   maskScienceFunc.init();
+   cfhtScienceMaskedImage.processPixels(maskScienceFunc);
+   std::cout << "Set " << maskScienceFunc.getCount() << " mask bits" << std::endl;
+
+   MaskedImage<ImagePixelType,MaskPixelType> cfhtTemplateMaskedImage;
+   cfhtTemplateMaskedImage.readFits(argv[2]);
+   cfhtTemplateMaskedImage.getMask()->addMaskPlane("manual");
+   // Can I reuse maskScienceFunc?
+   setMaskPixProcFunc<ImagePixelType, MaskPixelType> maskTemplateFunc(cfhtTemplateMaskedImage);
+   maskTemplateFunc.init();
+   cfhtTemplateMaskedImage.processPixels(maskTemplateFunc);
+   std::cout << "Set " << maskTemplateFunc.getCount() << " mask bits" << std::endl;
+
+   return 1;
 }
