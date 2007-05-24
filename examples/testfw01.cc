df8045d12a65e3f6886aa2c8ddacb6658949568f
becker <becker@git.lsstcorp.org>
2007-05-24 20:52:13 +0000

    code to test fw/.  ACB.

---
+++ examples/testfw01.cc
@@ -0,0 +1,16 @@
+// -*- lsst-c++ -*-
+#include "lsst/fw/MaskedImage.h"
+#include "lsst/fw/Trace.h"
+#include "lsst/fw/DiskImageResourceFITS.h"
+#include "lsst/fw/DataProperty.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace lsst::fw;
+
+int main( int argc, char** argv )
+{
+    ;
+}
+
