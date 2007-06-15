a673c8082c3829283b3ab559de5ecd85cb3cc54f
becker <becker@git.lsstcorp.org>
2007-06-15 01:06:11 +0000

    matrix inversion code works.  next up, SVD.

--- tests/invertMatrix.cc
+++ tests/invertMatrix.cc
@@ -2,8 +2,10 @@
 #include <boost/numeric/ublas/io.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/numeric/ublas/vector_proxy.hpp> 
 #include <boost/numeric/ublas/triangular.hpp>
 #include <boost/numeric/ublas/lu.hpp>
+#include "lsst/fw/Exception.h"
 
 using namespace boost::numeric;
 using namespace std;
@@ -14,36 +16,48 @@ int main()
     typedef ublas::matrix<double> Matrix;
 
     /* just a stupid test matrix */
-    Matrix m(3,3);
-    Matrix inverse(3,3);
+    Matrix m(3,3), inverse(3,3), p(3,3);
 
     for (unsigned i = 0; i < 3; ++ i)
         for (unsigned j = 0; j < 3; ++ j)
             m (i, j) = 3 * i + j + 2;
     
-    //Matrix I3 = ublas::identity_matrix<double>(3, 3);
-    //m = m + 3 * I3; // now m is not singular
+    Matrix I3 = ublas::identity_matrix<double>(3, 3);
+    m = m + 3 * I3; // now m is not singular
     
+    // BELOW IS THE GUTS OF THE CODE 
+    //
+    //
     // create a working copy of the input
-    Matrix<double> A(m);
+    Matrix A(m);
     
     // create a permutation matrix for the LU-factorization
     ublas::permutation_matrix<std::size_t> pm(A.size1());
 
-    // perform LU-factorization
-    int res = lu_factorize(A, pm);
-    if( res != 0 ) return false;
-    
+    // perform LU-factorization; for LSST throw exception instead of return
+    try {
+        int res = lu_factorize(A, pm);
+        if( res != 0 ) throw lsst::fw::Memory("LU factorization failed; singular matrix.  Get a better exception than this one!");
+    } catch (lsst::fw::Exception &e) {
+        cerr << "Caught exception: " << e.what() << "\n";
+        return 1;
+    }
+
     // create identity matrix of "inverse"
     inverse.assign(ublas::identity_matrix<double>(A.size1()));
     
     // backsubstitute to get the inverse
     lu_substitute(A, pm, inverse);
     
-    // can i do this?
-    Matrix p(m * pm);
+    //
+    //
+    // ABOVE IS THE GUTS OF THE CODE 
+
+    // verify you get out the identity matrix
+    p = ublas::prod< ublas::matrix<double> >(m,inverse); 
+
     cout << " input = " << m << endl;
-    cout << " inverse = " << pm << endl;
+    cout << " inverse = " << inverse << endl;
     cout << " product = " << p << endl;
     
     return 0;
--- tests/lsstimageproc03.cc
+++ tests/lsstimageproc03.cc
@@ -2,30 +2,138 @@
 #include "lsst/fw/Trace.h"
 #include "lsst/fw/Kernel.h"
 
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/lu.hpp>
+
 using namespace std;
 using namespace lsst::fw;
 
 // This piece of code includes the use case names as subroutines.
 void getTemplateChunkExposureFromTemplateExposure() {
-   wcsMatchExposure();
+    wcsMatchExposure();
 }
 void wcsMatchExposure() {
 }
 
 void PSFMatchMaskedImage() {
-   getCollectionOfMaskedImagesForPSFMatching();
-   computePSFMatchingKernelForEachMaskedImage();
-   computeSpatiallyVaryingPSFMatchingKernel();
+    getCollectionOfMaskedImagesForPSFMatching();
+
+    // Reusable view around each object
+    MaskedImage<ImagePixelType,MaskPixelType>::MaskedImagePtrT convolvePtr;
+    MaskedImage<ImagePixelType,MaskPixelType>::MaskedImagePtrT nonconvolvePtr;
+
+    // Output kernels
+    vector<boost::shared_ptr<Kernel<kernelPixelType> > > kernelVec;
+
+    // Iterate over object; use iterator instead?
+    for (unsigned nobj = 0; nobj < objectCollection.size(); nobj++) {
+        Object<XXX> diffImObject = objectCollection[nobj];
+        
+        // grab view around each object
+        // do i really want a new stamp or just a view?
+        BBox2i stamp(diffImObject.rowc - diffImObject.drow, diffImObject.rowc + diffImObject.drow, 
+                     diffImObject.colc - diffImObject.dcol, diffImObject.colc + diffImObject.dcol);
+        convolvePtr    = convolveMaskedImage.getSubImage(stamp);
+        nonconvolvePtr = nonconvolveMaskedImage.getSubImage(stamp);
+
+        boost::shared_ptr<lsst::fw::Kernel<pixelType> > kernelPtr(
+            new Kernel<kernelPixelType>()
+            );
+
+        computePSFMatchingKernelForEachMaskedImage(convolvePtr, nonconvolvePtr, deltaFunctionKernelSet, kernelPtr);
+        kernelVec.push_back(kernelPtr);
+    }
+
+
+    computeSpatiallyVaryingPSFMatchingKernel();
 }
 void getCollectionOfMaskedImagesForPSFMatching() {
 }
-void computePSFMatchingKernelForEachMaskedImage() {
+
+void computePSFMatchingKernelForEachMaskedImage(MaskedImagePtrT convolvePtr,
+                                                MaskedImagePtrT nonconvolvePtr,
+                                                LinearCombinationKernel deltaFunctionKernelSet,
+                                                boost::shared_ptr kernelVec) {
+    boost::numeric::ublas::vector<double> B(XXX);
+    boost::numeric::ublas::matrix<double> M(XXX, XXX);
+
+    // integral over image's dx and dy
+    for (unsigned row = 0; row < convolvePtr->getImage().nrow; row++) {
+        for (unsigned col = 0; col < convolvePtr->getImage().ncol; col++) {
+
+            double ncCamera   = nonconvolvePtr[row][col].camera();
+            double ncVariance = nonconvolvePtr[row][col].variance();
+
+            // kernel index i
+            for (unsigned kerni = 0; kerni < deltaFunctionKernelSet.size(); kerni++) {
+                kerneli = deltaFunctionKernelSet[kerni];
+
+                cData   = convolvePtr[row][col];
+                // This is effectively what we want to happen with the delta function kernel.
+                // cData = convolvePtr[row + kerneli->drow][col + kerneli->dcol];
+                // The more general case is
+                // convolve
+                boost::shared_ptr<MaskedImage<ImagePixelType, MaskPixelType> >
+                    cImagePtri = Kernel::convolve(convolvePtr[row][col], kerneli, threshold, vw::NoEdgeExtension(), -1);
+                
+                B[kerneli] += ncData.camera() * cImagePtri.camera() / (ncData.variance() + cImagePtri.variance());
+                
+                // background term (is this cData right?)
+                M[kerneli][nkernel] += cImagePtri.camera() / (cData.variance() + cImagePtri.variance());
+                
+                // kernel index j 
+                for (unsigned kernj = 0; kernj < deltaFunctionSet.size(); kernj++) {
+                    kernelj = deltaFunctionSet[kernj];
+                    
+                    // convolve
+                    boost::shared_ptr<MaskedImage<ImagePixelType, MaskPixelType> >
+                        cImagePtrj = Kernel::convolve(convolvePtr[row][col], kernelj, threshold, vw::NoEdgeExtension(), -1);
+                    
+                    M[kerni][kernj] += cImagePtri.camera() * cImagePtrj.camera() / (cImagePtri.variance() * cImagePtrj.variance());
+                }
+            }
+            
+            // background
+            M[nkernel][nkernel] += 1. / (ncData.variance() + cData.variance());
+            B[knernel]          += ncData.camera() / (ncData.variance() + cData.variance());
+        } // col
+    } // row
+    
+    vector LUv;
+    matrix LUm = LUD(LUv, M);
+    
+    vector solution;
+    LUSolve(LUm, B, LUv);
+
+
+    // create a working copy of the input
+    matrix<T> I(M);
+ 
+    // create a permutation matrix for the LU-factorization
+    boost::numeric::ublas::permutation_matrix<std::size_t> pm(I.size1());
+    
+    // perform LU-factorization
+    int res = lu_factorize(I,pm);
+    if( res != 0 ) return false;
+    
+    // create identity matrix of "inverse"
+    inverse.assign(boost::numeric::ublas::identity_matrix<pixelType>(I.size1()));
+    
+    // backsubstitute to get the inverse
+    lu_substitute(I, pm, inverse);
+
+
+
+    // rearrange vector into kernel Image
+    kernelVec = LUv;
 }
+
 void computeSpatiallyVaryingPSFMatchingKernel() {
-   fitKernelsUsingPrincipalComponentAnalysis();
+    fitKernelsUsingPrincipalComponentAnalysis();
 }
 void fitKernelsUsingPrincipalComponentAnalysis() {
-   fitArraysUsingPrincipalComponentAnalysis();
+    fitArraysUsingPrincipalComponentAnalysis();
 }
 void fitArraysUsingPrincipalComponentAnalysis() {
 }
@@ -33,16 +141,43 @@ void fitArraysUsingPrincipalComponentAnalysis() {
 
 int main( int argc, char** argv )
 {
-   // This is effectively Subtract Template From Science Chunk Exposure
-
+    // This is effectively Subtract Template From Science Chunk Exposure
+    
     Trace::setDestination(cout);
     Trace::setVerbosity(".", 0);
     
-    typedef uint8 MaskPixelType;
-    typedef float32 ImagePixelType;
+    typedef uint8 maskPixelType;
+    typedef float32 imagePixelType;
+    typedef float32 kernelPixelType;
 
+    // set up basis of delta functions for kernel
+    vector<boost::shared_ptr<Kernel<kernelPixelType> > > kernelVec;
+    int kernelRows = 9;
+    int kernelCols = 9;
+    int colCtr = (kernelCols - 1) / 2;
+    int rowCtr = (kernelRows - 1) / 2;
+    for (unsigned row = 0; row < kernelRows; ++row) {
+        int y = static_cast<int>(row) - rowCtr;
+        
+        for (unsigned col = 0; col < kernelCols; ++col) {
+            int x = static_cast<int>(col) - colCtr;
+
+            Kernel<kernelPixelType>::Function2PtrType kfuncPtr(
+                new IntegerDeltaFunction2<kernelPixelType>(x, y)
+            );
+            boost::shared_ptr<Kernel<kernelPixelType> > kernelPtr(
+                new AnalyticKernel<kernelPixelType>(kfuncPtr, kernelCols, kernelRows)
+            );
+            kernelVec.push_back(kernelPtr);
+
+        }
+    }
+    vector<imagePixelType> kernelParams(kernelRows * kernelCols);
+    LinearCombinationKernel<KernelPixelType> deltaFunctionKernelSet(kernelVec, kernelParams);
+    
+    
     getTemplateChunkExposureFromTemplateExposure();
     PSFMatchMaskedImage();
     subtractMatchedImage();
-
+    
 }
