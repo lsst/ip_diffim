14d337e01db5178883bf1490da900b9bb15abe85
becker <becker@git.lsstcorp.org>
2007-07-23 21:39:09 +0000

    Starting to test Minuit

---
+++ tests/testMinuit.cc
@@ -0,0 +1,11 @@
+#include "Minuit/FunctionMinimum.h"
+#include "Minuit/MnUserParameterState.h"
+#include "Minuit/MnPrint.h"
+#include "Minuit/MnMigrad.h"
+#include "Minuit/MnMinos.h"
+#include "Minuit/MnContours.h"
+#include "Minuit/MnPlot.h"
+#include "Minuit/MinosError.h"
+#include "Minuit/ContoursError.h"
+
+#include <iostream>
