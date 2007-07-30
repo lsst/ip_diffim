6a30edc283fa79d6039241c73924a4bd941b6790
becker <becker@git.lsstcorp.org>
2007-07-30 22:20:58 +0000

    Added additional attributes to correspond to DB schema.

--- include/lsst/fw/Source.h
+++ include/lsst/fw/Source.h
@@ -37,6 +37,64 @@ namespace fw {
        double _rowc;
        double _dcol;
        double _drow;
+
+       // Below is LSST DB Schema
+       long int _sourceId;
+       long int _ampExposureId;
+       short int _filterId;
+       long int _objectId;
+       long int _movingObjectId;
+       int _procHistoryId;
+       double _ra;
+       double _decl;
+       int ___zoneId_placeholder;
+       double _raErr4wcs;
+       double _decErr4wcs;
+       double _raErr4detection;
+       double _decErr4detection;
+       // _xpos DECIMAL(7,2) NOT NULL; // _rowc above
+       // _ypos DECIMAL(7,2) NOT NULL; // _colc above
+       double _rowcErr;                // WAS : _xposErr DECIMAL(4,2) NOT NULL;
+       double _colcErr;                // WAS : _yposErr DECIMAL(4,2) NOT NULL;
+       double _cx;
+       double _cy;
+       double _cz;
+       double _taiMidPoint;
+       double _taiRange;
+       float _fwhmA;
+       float _fwhmB;
+       float _fwhmTheta;
+       double _flux;
+       double _fluxErr;
+       float _psfMag;
+       float _psfMagErr;
+       float _apMag;
+       float _apMagErr;
+       float _apDia;
+       float _petroMag;
+       float _petroMagErr;
+       float _snr;
+       float _chi2;
+       float _sky;
+       float _skyErr;
+       float _moment0;
+       float _moment1_x;
+       float _moment1_y;
+       float _moment2_xx;
+       float _moment2_xy;
+       float _moment2_yy;
+       float _moment3_xxx;
+       float _moment3_xxy;
+       float _moment3_xyy;
+       float _moment3_yyy;
+       float _moment4_xxxx;
+       float _moment4_xxxy;
+       float _moment4_xxyy;
+       float _moment4_xyyy;
+       float _moment4_yyyy;
+       short int _flag4association;
+       short int _flag4detection;
+       short int _flag4wcs;
     };
     
 }   // namespace fw
