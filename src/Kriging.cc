21184a69db78fa286a60a751f10b7363cf03383a
becker <becker@git.lsstcorp.org>
2008-07-19 00:16:30 +0000

    added in gstat subroutines.

--- src/Kriging.cc
+++ src/Kriging.cc
@@ -17,10 +17,15 @@
 #include <glvars.h>
 */
 #include <lsst/ip/diffim/Kriging.h>
+
+#include <math.h>
+
 #include <select.h>
 #include <sem.h>
 #include <direct.h>
 #include <fit.h>
+#include <random.h>
+#include <mapio.h>
 
 using namespace std;
 
@@ -139,16 +144,226 @@ void lsst::ip::diffim::Variogram::fillData(int plane,
 }
 
 void lsst::ip::diffim::Variogram::transform_data(DATA *d) {
+    /* data.c */
+    Double_index *values;
+    double nscore, q;
+    FILE *f = NULL;
+    int i, j, tie_length, tie_total = 0;
+    DPOINT *p;
+    
+    if (d->log) {
+        for (i = 0; i < d->n_list; i++) {
+            p = d->list[i];
+            if (p->attr <= 0.0) 
+                //ErrMsg(ER_IMPOSVAL, "log of non-positive value");
+                ;
+            p->attr = log(p->attr);
+        }
+    } else if (! is_mv_double(&(d->Icutoff))) {
+        for (i = 0; i < d->n_list; i++) {
+            p = d->list[i];
+            if (p->attr <= d->Icutoff)
+                p->attr = 1.0;
+            else
+                p->attr = 0.0;
+        }
+    } else if (d->nscore_table) {
+        if (strlen(d->nscore_table) > 0) {
+            f = efopen(d->nscore_table, "w");
+            fprintf(f, "normal scores for %s\n", d->variable);
+            fprintf(f, "2\nobserved value\nnormal score\n");
+        }
+        /* ACB */
+        values = (Double_index *) emalloc(d->n_list * sizeof(Double_index));
+        for (i = 0; i < d->n_list; i++) {
+            values[i].d = d->list[i]->attr; /* */
+            values[i].index = i; /* unique identifiers */
+        }
+        qsort((void *) values, (size_t) d->n_list, sizeof(Double_index),
+              (int CDECL (*)(const void *, const void *)) double_index_cmp);
+        for (i = 0; i < d->n_list; i += tie_length) { /* ignore ties: */
+            /* assume they're all ties, with min run length 1: */
+            tie_length = 1;
+            while (i + tie_length < d->n_list && 
+                   values[i].d == values[i+tie_length].d)
+                tie_length++;
+            q = (i + 0.5 * tie_length) / d->n_list;
+            nscore = q_normal(q);
+            for (j = 0; j < tie_length; j++) {
+                if (f)
+                    fprintf(f, "%g %g\n", d->list[values[i+j].index]->attr, 
+                            nscore);
+                /* transform: */
+                d->list[values[i+j].index]->attr = nscore;
+            }
+            tie_total += (tie_length - 1);
+        }
+        efree(values);
+        if (f)
+            efclose(f);
+        if (tie_total)
+            //pr_warning("tied normal scores are assigned to tied data (%.2g%%)", 
+            //100.0 * tie_total / d->n_list);
+            ;
+    }
 }
+
 void lsst::ip::diffim::Variogram::mk_var_names(DATA *d) {
+    /* data.c */
+    char tmp1[100], tmp2[100]; 
+    /* make variable names, if not read from EAS header: */
+    if (d->variable == NULL || d->variable[0] == '\0')
+        sprintf(tmp1, "col[%d]", d->colnvalue);
+    else {
+        if (strlen(d->variable) > 50)
+            d->variable[50] = '\0';
+        sprintf(tmp1, "%s", d->variable);
+    }
+    if (d->log)
+        sprintf(tmp2, "log(%s)", tmp1);
+    else if (!is_mv_double(&(d->Icutoff)))
+        sprintf(tmp2, "I(%s,%g)", tmp1, d->Icutoff);
+    else if (d->Category)
+        sprintf(tmp2, "I(%s='%s')", tmp1, d->Category);
+    else if (d->nscore_table != NULL)
+        sprintf(tmp2, "%s (nscores)", tmp1);
+    else /* no transform, copy: */
+        sprintf(tmp2, "%s", tmp1);
+    d->variable = string_dup(tmp2);
+    if (d->x_coord == NULL && d->colnx) {
+        sprintf(tmp1, "x_%d", d->colnx);
+        d->x_coord = string_dup(tmp1);
+    }
+    if (d->y_coord == NULL && d->colny) {
+        sprintf(tmp1, "y_%d", d->colny);
+        d->y_coord = string_dup(tmp1);
+    }
+    if (d->z_coord == NULL && d->colnz)  {
+        sprintf(tmp1, "z_%d", d->colnz);
+        d->z_coord = string_dup(tmp1);
+    }
+    if (d->V_coord == NULL && d->colnvariance)  {
+        sprintf(tmp1, "var_%d", d->colnvariance);
+        d->V_coord = string_dup(tmp1);
+    }
+    if (d->s_coord == NULL && d->colns) {
+        sprintf(tmp1, "stra_%d", d->colns);
+        d->s_coord = string_dup(tmp1);
+    }
+    if (d->id_name == NULL && d->coln_id) {
+        sprintf(tmp1, "ID_%d", d->colns);
+        d->id_name = string_dup(tmp1);
+    }
 }
+
 int lsst::ip::diffim::Variogram::average_duplicates(DATA *d) {
+    /* data.c */
+
+    /*
+     * average duplicate records (equal coordinates) 
+     * and correct number of records;
+     * new variance = (sum variances)/(n * n)
+     */
+    int n_tot = 0, n_dupl, i, j;
+    double dzero2;
+    
+    if (! d->average)
+        return 0;
+    dzero2 = gl_zero * gl_zero;
+    for (i = 0; i < d->n_list; i++) {
+        n_dupl = 0;
+        j = i + 1;
+        while (j < d->n_list) { /* can't use a for() loop here */
+            if (d->pp_norm2(d->list[i], d->list[j]) < dzero2) {
+                d->list[i]->attr += d->list[j]->attr;
+                d->list[i]->variance += d->list[j]->variance;
+                pop_point(d, j); /* decrements d->n_list */
+                n_dupl++;
+            } else 
+                j++;
+        } /* while j */
+        if (n_dupl > 0) {
+            n_tot += n_dupl;
+            d->list[i]->attr /= (n_dupl + 1.0);
+            d->list[i]->variance /= SQR(n_dupl + 1.0);
+        }
+    }
+    return d->n_averaged = n_tot;
 }
+
 void lsst::ip::diffim::Variogram::calc_data_mean_std(DATA *d) {
+    /* data.c */
+    
+    /* 
+     * Calculates fields mean and std of d with mean and standard dev. (/(n-1))
+     */
+    int i;
+    
+    if (d->standard == 2) { /* we did this already.. */
+        for (i = 0; i < d->n_list; i++)
+            d->list[i]->attr *= d->std;
+    }
+    
+    d->mean = 0.0; 
+    d->std = 0.0;
+    if (d->n_list <= 0) {
+        //pr_warning("calc_data_mean_std: n_list <= 0: %d", d->n_list);
+        return;
+    }
+    for (i = 0; i < d->n_list; i++) 
+        d->mean += d->list[i]->attr;
+    d->mean /= d->n_list;
+    if (d->n_list == 1)
+        return;
+    for (i = 0; i < d->n_list; i++) 
+        d->std += SQR(d->list[i]->attr - d->mean);
+    d->std = sqrt((d->std)/(d->n_list - 1));
+    
+    if (d->standard > 0) {
+        for (i = 0; i < d->n_list; i++)
+            d->list[i]->attr /= d->std;
+        d->standard = 2;
+    }
+    return;
 }
+
 void lsst::ip::diffim::Variogram::correct_strata(DATA *d) {
+    /* data.c */
+
+    int i;
+    
+    if (d->colns == 0)
+        return;
+    for (i = 0; i < d->n_list; i++)
+        d->list[i]->u.stratum -= d->minstratum;
 }
+
 void lsst::ip::diffim::Variogram::ev2map(VARIOGRAM *v) {
+    /* sem.c */
+    
+    GRIDMAP *m1 = NULL, *m2 = NULL;
+    unsigned int row, col, i;
+    SAMPLE_VGM *ev;
+    
+    if (v->fname == NULL)
+        return;
+    ev = v->ev;
+    m1 = map_dup(v->fname, ev->map);
+    if (v->fname2 != NULL)
+        m2 = map_dup(v->fname2, ev->map);
+    for (row = i = 0; row < m1->rows; row++) {
+        for (col = 0; col < m1->cols; col++) {
+            if (ev->nh[i] > 0)
+                map_put_cell(m1, row, col, ev->gamma[i]);
+            if (m2 != NULL)
+                map_put_cell(m2, row, col, 1.0 * ev->nh[i]);
+            i++;
+        }
+    }
+    m1->write(m1);
+    if (m2 != NULL)
+        m2->write(m2);
+    return;
 }
 
 //int lsst::ip::diffim::Variogram::calcVariogram() {
