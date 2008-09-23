4eec9cc16bf6800bc80f81cd39916e251db2b509
becker <becker@git.lsstcorp.org>
2008-09-23 17:47:19 +0000

    forgot example file.

---
+++ examples/difi.cc
@@ -0,0 +1,30 @@
+#include "lsst/detection/Footprint.h"
+#include "lsst/ip/diffim/ImageSubtract.h"
+
+using namespace lsst::afw::image;
+using namespace lsst::ip::diffim;
+typedef float ImagePixelT;
+
+int main() {
+    typedef DifferenceImageFootprintInformation<ImagePixelT, lsst::afw::image::maskPixelType> Difi;
+
+    boost::shared_ptr<MaskedImage<ImagePixelT, lsst::afw::image::maskPixelType> > miPtr (
+        new MaskedImage<ImagePixelT, lsst::afw::image::maskPixelType>(100,100)
+        );
+
+    vw::BBox2i const region(10,10,5,5);
+    lsst::detection::Footprint::PtrType fpPtr(
+        new lsst::detection::Footprint(region)
+        ); 
+    
+    Difi difi(fpPtr, miPtr, miPtr);
+    Difi::PtrType difiPtr (new Difi(fpPtr, miPtr, miPtr));
+
+    std::vector<Difi::PtrType> difiList;
+    difiList.push_back(difiPtr);
+
+    Difi::PtrType difiList2 = getGoodFootprints(difiList);
+
+    return 0;
+}
+
