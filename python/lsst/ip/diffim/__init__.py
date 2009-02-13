603fe06257e102e972cc986b97058da1a412a7e7
becker <becker@git.lsstcorp.org>
2009-02-13 18:02:22 +0000

    Cleaning up python directory.  #509.

--- python/lsst/ip/diffim/__init__.py
+++ python/lsst/ip/diffim/__init__.py
@@ -1,1 +1,10 @@
+# c wrapper
 from diffimLib import *
+
+# python code
+from diffimTools import *
+from spatialKernelFit import *
+
+# optional
+# from diffimDebug import *
+# from diffimPlot import *
