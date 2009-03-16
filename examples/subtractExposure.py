2fd65fd7756f17eb78eaf5e8b90ebefcf6896156
becker <becker@git.lsstcorp.org>
2009-03-16 22:47:49 +0000

    moved core functionality to diffimStages.py

--- examples/subtractExposure.py
+++ examples/subtractExposure.py
@@ -9,46 +9,7 @@ from lsst.pex.logging import Trace
 from lsst.pex.policy import Policy
 from lsst.pex.logging import Log
 
-from subtractMaskedImage import subtractMaskedImage
-
-def subtractExposure(templateExposure, scienceExposure, policy):
-    # Make sure they end up the same dimensions on the sky
-    templateWcs      = templateExposure.getWcs() 
-    scienceWcs       = scienceExposure.getWcs()
-
-    templateMaskedImage = templateExposure.getMaskedImage()
-    scienceMaskedImage  = scienceExposure.getMaskedImage()
-
-    templateOrigin   = templateWcs.xyToRaDec(0,0)
-    scienceOrigin    = scienceWcs.xyToRaDec(0,0)
-    # Within some tolerance; do we have sky distance methods?
-    #assert(templateOrigin[0] == scienceOrigin[0])
-    #assert(templateOrigin[1] == scienceOrigin[1])
-    assert(templateOrigin == scienceOrigin)
-
-    templateLimit    = templateWcs.xyToRaDec(templateMaskedImage.getHeight(),
-                                             templateMaskedImage.getWidth())
-    scienceLimit     = scienceWcs.xyToRaDec(scienceMaskedImage.getHeight(),
-                                            scienceMaskedImage.getWidth())
-    # Within some tolerance; do we have sky distance methods?
-    #assert(templateLimit[0]  == scienceLimit[0])
-    #assert(templateLimit[1]  == scienceLimit[1])
-    assert(templateLimit  == scienceLimit)
-
-    # Make sure they end up the EXACT same dimensions in pixels
-    # This is non-negotiable
-    assert (templateMaskedImage.getDimensions() == scienceMaskedImage.getDimensions())
-
-    # Subtract their MaskedImages
-    differenceMaskedImage, spatialKernel, backgroundModel, sdqaList = subtractMaskedImage(templateMaskedImage,
-                                                                                          scienceMaskedImage,
-                                                                                          policy)
-    # Note : we assume that the Template is warped to the science image's WCS
-    #      : meaning that the scienceWcs is the correct one to store in the diffim
-    differenceExposure = afwImage.ExposureF(differenceMaskedImage, scienceWcs)
-
-    return differenceExposure, spatialKernel, backgroundModel, sdqaList
-
+from lsst.ip.diffim.diffimStages import subtractExposure
 
 def main():
     defDataDir   = eups.productDir('afwdata')
