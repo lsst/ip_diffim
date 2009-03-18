ba9d7dad1681c93998823830b1b5e5139933c8ef
rowen <rowen@git.lsstcorp.org>
2009-03-18 22:50:58 +0000

    computeTemplateBbox example: added the ability to write out the sub-template image

--- examples/computeTemplateBbox.py
+++ examples/computeTemplateBbox.py
@@ -19,7 +19,7 @@ def getArg(ind, defValue):
 def printBBox(descr, bbox):
     print "%s: [%s, %s], [%s, %s]" % (descr, bbox.getX0(), bbox.getY0(), bbox.getX1(), bbox.getY1())
 
-def computeBBox(scienceImagePath, templateImagePath, borderWidth):
+def computeBBox(scienceImagePath, templateImagePath, borderWidth, templateSubImagePath=None):
     """Compute and print template bounding box
     
     Inputs:
@@ -34,14 +34,21 @@ def computeBBox(scienceImagePath, templateImagePath, borderWidth):
     sciBBox = afwImage.BBox(afwImage.PointI(0, 0), sciDim[0], sciDim[1])
     printBBox("Science  BBox", sciBBox)
     
-    tmpHdrData = dafBase.PropertySet()
-    tmpIm = afwImage.ImageF(templateImagePath, 0, tmpHdrData)
+    tmpHdrData = afwImage.readMetadata(templateImagePath)
+    tmpDim = afwImage.PointI(tmpHdrData.getInt("NAXIS1"), tmpHdrData.getInt("NAXIS2"))
     tmpWcs = afwImage.Wcs(tmpHdrData)
-    tmpDim = tmpIm.getDimensions()
     
     borderWidth = 5
     tmpBBox = ipDiffim.computeTemplateBbox(sciWcs, sciDim, tmpWcs, tmpDim, borderWidth)
     printBBox("Template BBox", tmpBBox)
+
+    print "Reading sub template exposure"
+    tmpSubHdr = dafBase.PropertySet()
+    tmpSubIm = afwImage.ImageF(templateImagePath, 0, tmpSubHdr, tmpBBox)
+    
+    if templateSubImagePath:
+        print "Saving templateSubImage to:", templateSubImagePath
+        tmpSubIm.writeFits(templateSubImagePath, tmpSubHdr)
 
 if __name__ == "__main__":
     dataDir = eups.productDir("afwdata")
@@ -52,11 +59,12 @@ if __name__ == "__main__":
         defScienceImagePath = os.path.join(dataDir, DefScienceImageName)
         defTemplateImagePath = os.path.join(dataDir, DefTemplateImageName)
 
-    usage = """usage: %%prog [options] [scienceImage templateImage
+    usage = """usage: %%prog [options] scienceImage templateImage [templateSubImage]
 
-    Computes bounding box on a template image corresponding to a science image
+    Computes bounding box on a template image corresponding to a science image.
+    If templateSubImage is specified, saves the corresponding template subimage as a FITS file.
     
-    Warning: the two FITS images must contain WCS information in their header
+    Warning: the two input FITS images must contain WCS information in their header
 
     Note:
     - image arguments are paths to Image fits files; they must include WCS information
@@ -77,6 +85,7 @@ if __name__ == "__main__":
 
     scienceImagePath = getArg(0, defScienceImagePath)
     templateImagePath = getArg(1, defTemplateImagePath)
+    templateSubImagePath = getArg(2, None)
     if None in (scienceImagePath, templateImagePath):
         print "Must setup afwdata or else provide image paths"
     borderWidth = opt.border
@@ -89,4 +98,6 @@ if __name__ == "__main__":
         print "Verbosity =", opt.verbosity
         lsst.pex.logging.Trace_setVerbosity("lsst.ip.diffim", opt.verbosity)
 
-    computeBBox(scienceImagePath, templateImagePath, borderWidth)
+    computeBBox(scienceImagePath, templateImagePath, borderWidth, templateSubImagePath)
+        
+    
