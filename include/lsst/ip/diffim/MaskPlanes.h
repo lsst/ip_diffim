3a3d06fde3411b6cc11083692a9b41d11785fd5f
rhl <rhl@git.lsstcorp.org>
2009-03-29 16:13:56 +0000

    Remove un-used plane definitions that repeat ones from afw

--- include/lsst/ip/diffim/MaskPlanes.h
+++ include/lsst/ip/diffim/MaskPlanes.h
@@ -1,25 +1,5 @@
 // -*- lsst-c++ -*-
 #ifndef LSST_Ip_Diffim_MaskPlanes_H
 #define LSST_Ip_Diffim_MaskPlanes_H
-
-#include <map>
-#include <string>
-
-typedef std::map<std::string, int> strIntMap;
-typedef std::pair<std::string, int> strIntPair;
-
-namespace lsst {
-namespace ip {
-namespace diffim {
-        
-    strIntMap MaskPlanes;
-    MaskPlanes.insert(strIntPair{"BAD",   0}); /* For non-LSST data, not entirely sure why data are bad */
-    MaskPlanes.insert(strIntPair{"SAT",   1}); /* Pixel is/was saturated */
-    MaskPlanes.insert(strIntPair{"INTRP", 2}); /* Pixel has been interpolated */
-    MaskPlanes.insert(strIntPair{"CR",    3}); /* Pixel is part of a cosmic ray */
-    MaskPlanes.insert(strIntPair{"EDGE",  4}); /* Pixel is too close to the edge of the image, e.g. during convolution */
-}
-}
-}
         
 #endif // !defined(LSST_Ip_Diffim_MaskPlanes_H)
