9cafeb4f08c24a6695893e8dab68e58ed3d6fcf3
becker <becker@git.lsstcorp.org>
2009-06-23 17:08:52 +0000

    Functioning unit test.

--- tests/ConvolveAndSubtract.py
+++ tests/ConvolveAndSubtract.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
-import os
-
+import os, pdb, sys
+import numpy as num
 import unittest
 import lsst.utils.tests as tests
 
 import eups
 import lsst.afw.detection as afwDetection
 import lsst.afw.image as afwImage
+import lsst.afw.math as afwMath
 import lsst.pex.policy as pexPolicy
 import lsst.ip.diffim as ipDiffim
+import lsst.ip.diffim.diffimTools as diffimTools
 import lsst.pex.logging as logging
 
 import lsst.afw.display.ds9 as ds9
@@ -19,17 +21,63 @@ logging.Trace_setVerbosity('lsst.ip.diffim', Verbosity)
 diffimDir    = eups.productDir('ip_diffim')
 diffimPolicy = os.path.join(diffimDir, 'pipeline', 'ImageSubtractStageDictionary.paf')
 
+# This one tests convolve and subtract
+
 class DiffimTestCases(unittest.TestCase):
     
+    # D = I - (K.x.T + bg)
+        
     def setUp(self):
-        self.policy = pexPolicy.Policy.createPolicy(diffimPolicy)
+        self.policy      = pexPolicy.Policy.createPolicy(diffimPolicy)
+        self.kCols       = self.policy.getInt('kernelCols')
+        self.kRows       = self.policy.getInt('kernelRows')
+
+        # gaussian reference kernel
+        self.gSize         = self.kCols
+        self.gaussFunction = afwMath.GaussianFunction2D(2, 3)
+        self.gaussKernel   = afwMath.AnalyticKernel(self.gSize, self.gSize, self.gaussFunction)
+
+        # edge bit
+        self.edgeBit       = afwImage.MaskU().getMaskPlane('EDGE')
+
+        # known input images
+        defDataDir = eups.productDir('afwdata')
+        defImagePath = os.path.join(defDataDir, "DC3a-Sim", "sci", "v5-e0",
+                                    "v5-e0-c011-a00.sci")
+        self.templateImage  = afwImage.MaskedImageF(defImagePath)
+        self.scienceImage   = self.templateImage.Factory( self.templateImage.getDimensions() )
+        
+        afwMath.convolve(self.scienceImage, self.templateImage, self.gaussKernel, False, self.edgeBit)
         
     def tearDown(self):
         del self.policy
 
-    def testThrow(self):
-        # THIS IS NOT FINISHED FOR DC3a
-        self.assertEqual(0,1)
+    def runConvolveAndSubtract(self, bg = 0, xloc = 408, yloc = 580):
+        imsize = int(5 * self.kCols)
+        
+        bbox = afwImage.BBox( afwImage.PointI(xloc - imsize/2,
+                                              yloc - imsize/2),
+                              afwImage.PointI(xloc + imsize/2,
+                                              yloc + imsize/2) )
+
+        tmi     = afwImage.MaskedImageF(self.templateImage, bbox)
+        smi     = afwImage.MaskedImageF(self.scienceImage, bbox)
+        diffIm  = ipDiffim.convolveAndSubtract(tmi, smi, self.gaussKernel, bg)
+
+        bbox    = afwImage.BBox(afwImage.PointI(self.gaussKernel.getCtrX(),
+                                                self.gaussKernel.getCtrY()) ,
+                                afwImage.PointI(imsize - (self.gaussKernel.getWidth()  - self.gaussKernel.getCtrX()),
+                                                imsize - (self.gaussKernel.getHeight() - self.gaussKernel.getCtrY())))
+        diffIm2 = afwImage.MaskedImageF(diffIm, bbox)
+
+        # image is empty (or the additional background you subtracted off)
+        for j in range(diffIm2.getHeight()):
+            for i in range(diffIm2.getWidth()):
+                self.assertAlmostEqual(diffIm2.getImage().get(i, j), -1.*bg, 4)
+
+    def testConvolveAndSubtract(self):
+        self.runConvolveAndSubtract(bg=0)
+        self.runConvolveAndSubtract(bg=10)
 
 #####
         
