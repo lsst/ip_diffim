e2d061ac6ecb42087cbd1f20d870d73106b9700f
becker <becker@git.lsstcorp.org>
2009-09-25 20:59:26 +0000

    Mirror python script that does the same as the C++ code.  #876.

--- examples/testSpatialCell.cc
+++ examples/testSpatialCell.cc
@@ -10,10 +10,6 @@ namespace afwImage = lsst::afw::image;
 namespace afwMath  = lsst::afw::math;
 namespace ipDiffim = lsst::ip::diffim;
 
-/*
- * Make this a subroutine so that locals go out of scope as part of test
- * of memory management
- */
 void test() {
     typedef float PixelT;
 
@@ -24,16 +20,9 @@ void test() {
     
     boost::shared_ptr<afwImage::MaskedImage<PixelT> > tmi( new afwImage::MaskedImage<PixelT>(stampSize, stampSize) );
     boost::shared_ptr<afwImage::MaskedImage<PixelT> > smi( new afwImage::MaskedImage<PixelT>(stampSize, stampSize) );
-    *tmi->getImage() = 0;
-    *tmi->getMask() = 0x0;
-    *tmi->getVariance() = 0;
-    *smi->getImage() = 0;
-    *smi->getMask() = 0x0;
-    *smi->getVariance() = 0;
-
 
     afwImage::BBox          bbox    = afwImage::BBox(afwImage::PointI(0, 0), fullSize, fullSize);
-    afwMath::SpatialCellSet cellSet = afwMath::SpatialCellSet(bbox, cellSize);
+    afwMath::SpatialCellSet cellSet = afwMath::SpatialCellSet(bbox, cellSize, cellSize);
 
     ipDiffim::KernelCandidate<PixelT>::Ptr cand = ipDiffim::makeKernelCandidate(coord, coord, tmi, smi);
     cellSet.insertCandidate(cand);
