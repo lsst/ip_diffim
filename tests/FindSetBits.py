902413d508eddffadcd2d2a1aa1c5577440a8683
becker <becker@git.lsstcorp.org>
2009-12-16 22:57:47 +0000

    builds against afw trunk r12158. #876

--- tests/FindSetBits.py
+++ tests/FindSetBits.py
@@ -30,12 +30,11 @@ class DiffimTestCases(unittest.TestCase):
     def testNoMask(self):
         mask = afwImage.MaskU(20,20)
         mask.set(0)
-        fsb  = ipDiffim.FindSetBitsU(mask)
+        fsb  = ipDiffim.FindSetBitsU()
 
         bbox     = afwImage.BBox(afwImage.PointI(0,10),
                                  afwImage.PointI(9,12))
-        fp       = afwDetection.Footprint(bbox)
-        fsb.apply(fp)
+        fsb.apply(afwImage.MaskU(mask, bbox))
 
         self.assertEqual(fsb.getBits(), 0)
 
@@ -43,7 +42,7 @@ class DiffimTestCases(unittest.TestCase):
         mask = afwImage.MaskU(20,20)
         mask.set(0)
         bitmaskBad = mask.getPlaneBitMask('BAD')
-        fsb = ipDiffim.FindSetBitsU(mask)
+        fsb = ipDiffim.FindSetBitsU()
 
         bbox     = afwImage.BBox(afwImage.PointI(9,10),
                                  afwImage.PointI(11,12))
@@ -52,8 +51,7 @@ class DiffimTestCases(unittest.TestCase):
 
         bbox2    = afwImage.BBox(afwImage.PointI(8,8),
                                  afwImage.PointI(19,19))
-        fp       = afwDetection.Footprint(bbox2)
-        fsb.apply(fp)
+        fsb.apply(afwImage.MaskU(mask, bbox2))
 
         self.assertEqual(fsb.getBits(), bitmaskBad)
 
@@ -62,7 +60,7 @@ class DiffimTestCases(unittest.TestCase):
         mask.set(0)
         bitmaskBad = mask.getPlaneBitMask('BAD')
         bitmaskSat = mask.getPlaneBitMask('SAT')
-        fsb = ipDiffim.FindSetBitsU(mask)
+        fsb = ipDiffim.FindSetBitsU()
 
         bbox      = afwImage.BBox(afwImage.PointI(9,10),
                                   afwImage.PointI(11,12))
@@ -77,7 +75,7 @@ class DiffimTestCases(unittest.TestCase):
         bbox3     = afwImage.BBox(afwImage.PointI(0,0),
                                   afwImage.PointI(19,19))
         fp       = afwDetection.Footprint(bbox3)
-        fsb.apply(fp)
+        fsb.apply(afwImage.MaskU(mask, bbox3))
 
         self.assertEqual(fsb.getBits(), bitmaskBad | bitmaskSat)
 
