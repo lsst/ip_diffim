6ee222b0ca9537921fdc0858bceae6a3075103ee
Andy Becker <acbecker@gmail.com>
2013-01-28 18:56:54 -0800

    Working makeDipole measurement script

--- examples/makeDipole.py
+++ examples/makeDipole.py
@@ -5,29 +5,34 @@ import lsst.afw.detection as afwDet
 import lsst.afw.geom as afwGeom
 import lsst.afw.table as afwTable
 import lsst.meas.algorithms as measAlg
+import lsst.ip.diffim as ipDiffim
 
 w,h = 100,100
+xc,yc=50,50
+
+# Make random noise image
 image = afwImage.MaskedImageF(w,h)
 image.set(0)
-
-xc,yc=50,50
 array = image.getImage().getArray()
 array[:,:] = numpy.random.randn(w,h)
+var   = image.getVariance()
+var.set(1.0)
 ds9.mtv(image, frame=1)
-
-
-# For the dipole
+ds9.mtv(image.getVariance(), frame=2)
+# Create Psf for dipole creation and measurement
 psf = afwDet.createPsf("DoubleGaussian", 11, 11, 1.0, 2.5, 0.1)
 psfim = psf.computeImage(afwGeom.Point2D(0., 0.)).convertF()
-psfim *= 100
 psfw, psfh = psfim.getDimensions()
 
+# For the dipole
 array = image.getImage().getArray()
-array[yc-psfw//2:yc-psfw//2+psfw, xc-psfw//2:xc-psfw//2+psfw] += psfim.getArray()
-array[yc-psfw//4:yc-psfw//4+psfh, xc-psfw//4:xc-psfw//4+psfw] -= psfim.getArray()
-      
-ds9.mtv(image, frame=2)
+x0, y0 = xc-psfw+psfw//3, yc-psfh+psfh//3
+array[y0:y0+psfh, x0:x0+psfw] += psfim.getArray() * 100 
+x0, y0 = xc-psfw//3, yc-psfh//3
+array[y0:y0+psfh, x0:x0+psfw] -= psfim.getArray() * 100
+ds9.mtv(image, frame=3)
 
+# Create an exposure, detect positive and negative peaks separately
 exp = afwImage.makeExposure(image)
 exp.setPsf(psf)
 config = measAlg.SourceDetectionConfig()
@@ -37,18 +42,42 @@ schema = afwTable.SourceTable.makeMinimalSchema()
 task = measAlg.SourceDetectionTask(schema, config=config)
 table = afwTable.SourceTable.make(schema)
 results = task.makeSourceCatalog(table, exp)
-ds9.mtv(image, frame=3)
+ds9.mtv(image, frame=4)
 
-print len(results.sources)
+# Merge them together
+print "# Before merge, nSrc =", len(results.sources)
 fpSet = results.fpSets.positive
 fpSet.merge(results.fpSets.negative, 0, 0, False)
 sources = afwTable.SourceCatalog(table)
 fpSet.makeSources(sources)
-print len(sources)
+print "# After merge, nSrc =", len(sources)
 s = sources[0]
-print len(s.getFootprint().getPeaks())
+print "# And nPeaks =", len(s.getFootprint().getPeaks())
+
+# Measure dipole at known location
+schema  = afwTable.SourceTable.makeMinimalSchema()
+control = ipDiffim.NaiveDipoleCentroidControl()
+msb     = measAlg.MeasureSourcesBuilder().addAlgorithm(control)
+ms      = msb.build(schema)
+table   = afwTable.SourceTable.make(schema)
+source  = table.makeRecord()
+
+# ms.apply(source, exp, afwGeom.Point2D(xc, yc))
+# Hmm throws Assertion `px != 0' failed.
+# Looks like this is because there is no getFootprint() yet...
+
+source.setFootprint(s.getFootprint())
+ms.apply(source, exp, afwGeom.Point2D(xc, yc))
+
+# Hmm, this also fails
+# print source[control.name]
+# KeyError: "Field 'centroid.dipole.naive' not found in Schema."
+# I think thats because the fields are 'centroid.dipole.naive.pos' and '.neg'
+
+for key in schema.getNames():
+    print key, source.get(key)
 
 
-
-
+#control = ipDiffim.NaiveDipoleFluxControl()
+#msb     = msb.addAlgorithm(control)
 
