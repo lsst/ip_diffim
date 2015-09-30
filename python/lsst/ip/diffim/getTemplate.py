dd403b30690d3c552f5b762e9c99a2871c5411d7
Yusra AlSayyad <yusra@astro.washington.edu>
2015-09-30 00:53:08 -0700

    Move getTemplate subtask to ip_diffim

---
+++ python/lsst/ip/diffim/getTemplate.py
@@ -0,0 +1,189 @@
+from __future__ import division, absolute_import
+#
+# LSST Data Management System
+# Copyright 2015 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+import numpy
+import lsst.pex.config as pexConfig
+import lsst.pipe.base as pipeBase
+import lsst.afw.geom as afwGeom
+import lsst.afw.image as afwImage
+
+__all__ = ["GetCoaddAsTemplateTask", "GetCoaddAsTemplateConfig",
+           "GetCalexpAsTemplateTask", "GetCalexpAsTemplateConfig"]
+
+class GetCoaddAsTemplateConfig(pexConfig.Config):
+    templateBorderSize = pexConfig.Field(
+        dtype = int,
+        default = 10,
+        doc = "Number of pixels to grow the requested template image to account for warping"
+        )
+    coaddName = pexConfig.Field(
+        doc = "coadd name: typically one of deep or goodSeeing",
+        dtype = str,
+        default = "deep",
+    )
+
+
+class GetCoaddAsTemplateTask(pipeBase.Task):
+    """Subtask to retrieve coadd for use as an image difference template.
+
+    This is the default getTemplate Task to be run as a subtask by
+    pipe.tasks.ImageDifferenceTask. The main method is run().
+    It assumes that coadds reside in the repository given by sensorRef.
+    """
+    ConfigClass = GetCoaddAsTemplateConfig
+    _DefaultName = "GetCoaddAsTemplateTask"
+
+    def run(self, exposure, sensorRef, templateIdList=None):
+        """!Retrieve and mosaic a template coadd exposure that overlaps the exposure
+
+        \param[in] exposure -- an exposure for which to generate an overlapping template
+        \param[in] sensorRef -- a Butler data reference that can be used to obtain coadd data
+        \param[in] templateIdList -- list of data ids (unused)
+
+        \return a pipeBase.Struct
+         - exposure: a template coadd exposure assembled out of patches
+         - sources:  None for this subtask
+        """
+        skyMap = sensorRef.get(datasetType=self.config.coaddName + "Coadd_skyMap")
+        expWcs = exposure.getWcs()
+        expBoxD = afwGeom.Box2D(exposure.getBBox())
+        expBoxD.grow(self.config.templateBorderSize)
+        ctrSkyPos = expWcs.pixelToSky(expBoxD.getCenter())
+        tractInfo = skyMap.findTract(ctrSkyPos)
+        self.log.info("Using skyMap tract %s" % (tractInfo.getId(),))
+        skyCorners = [expWcs.pixelToSky(pixPos) for pixPos in expBoxD.getCorners()]
+        patchList = tractInfo.findPatchList(skyCorners)
+
+        if not patchList:
+            raise RuntimeError("No suitable tract found")
+        self.log.info("Assembling %s coadd patches" % (len(patchList),))
+
+        # compute coadd bbox
+        coaddWcs = tractInfo.getWcs()
+        coaddBBox = afwGeom.Box2D()
+        for skyPos in skyCorners:
+            coaddBBox.include(coaddWcs.skyToPixel(skyPos))
+        coaddBBox = afwGeom.Box2I(coaddBBox)
+        self.log.info("exposure dimensions=%s; coadd dimensions=%s" %
+                      (exposure.getDimensions(), coaddBBox.getDimensions()))
+
+        # assemble coadd exposure from subregions of patches
+        coaddExposure = afwImage.ExposureF(coaddBBox, coaddWcs)
+        coaddExposure.getMaskedImage().set(numpy.nan, afwImage.MaskU.getPlaneBitMask("NO_DATA"), numpy.nan)
+        nPatchesFound = 0
+        coaddFilter = None
+        coaddPsf = None
+        for patchInfo in patchList:
+            patchSubBBox = patchInfo.getOuterBBox()
+            patchSubBBox.clip(coaddBBox)
+            patchArgDict = dict(
+                datasetType=self.config.coaddName + "Coadd_sub",
+                bbox=patchSubBBox,
+                tract=tractInfo.getId(),
+                patch="%s,%s" % (patchInfo.getIndex()[0], patchInfo.getIndex()[1]),
+            )
+            if patchSubBBox.isEmpty():
+                self.log.info("skip tract=%(tract)s, patch=%(patch)s; no overlapping pixels" % patchArgDict)
+                continue
+            if not sensorRef.datasetExists(**patchArgDict):
+                self.log.warn("%(datasetType)s, tract=%(tract)s, patch=%(patch)s does not exist"
+                              % patchArgDict)
+                continue
+
+            nPatchesFound += 1
+            self.log.info("Reading patch %s" % patchArgDict)
+            coaddPatch = sensorRef.get(**patchArgDict)
+            coaddView = afwImage.MaskedImageF(coaddExposure.getMaskedImage(), coaddPatch.getBBox())
+            coaddView <<= coaddPatch.getMaskedImage()
+            if coaddFilter is None:
+                coaddFilter = coaddPatch.getFilter()
+
+            # Retrieve the PSF for this coadd tract, if not already retrieved
+            if coaddPsf is None and coaddPatch.hasPsf():
+                coaddPsf = coaddPatch.getPsf()
+
+        if nPatchesFound == 0:
+            raise RuntimeError("No patches found!")
+
+        if coaddPsf is None:
+            raise RuntimeError("No coadd Psf found!")
+
+        coaddExposure.setPsf(coaddPsf)
+        coaddExposure.setFilter(coaddFilter)
+        return pipeBase.Struct(exposure = coaddExposure,
+                               sources = None)
+
+
+class GetCalexpAsTemplateConfig(pexConfig.Config):
+    doAddCalexpBackground = pexConfig.Field(
+        dtype=bool,
+        default=True,
+        doc="Add background to calexp before processing it."
+        )
+
+
+class GetCalexpAsTemplateTask(pipeBase.Task):
+    """Subtask to retrieve calexp of the same ccd number as the science image SensorRef
+    for use as an image difference template.
+
+    To be run as a subtask by pipe.tasks.ImageDifferenceTask.
+    Intended for use with simulations and surveys that repeatedly visit the same pointing.
+    This code was originally part of Winter2013ImageDifferenceTask.
+    """
+
+    ConfigClass = GetCalexpAsTemplateConfig
+    _DefaultName = "GetCalexpAsTemplateTask"
+
+    def run(self, exposure, sensorRef, templateIdList):
+        """!Return a calexp exposure with same ccd as input sensorRef.
+
+        \param[in] exposure -- exposure (unused)
+        \param[in] sensorRef -- a Butler data reference
+        \param[in] templateIdList -- list of data ids. Only visit used.
+
+        \return a pipeBase.Struct
+         - exposure: a template calexp
+         - sources: source catalog measured on the template
+        """
+
+        if len(templateIdList) > 1:
+            self.log.warn("Multiple template visits supplied. Getting template from first visit: %s" %
+                          (templateIdList[0]['visit']))
+
+        templateId = sensorRef.dataId.copy()
+        templateId["visit"] = templateIdList[0]['visit']
+        self.log.info("Fetching calexp (%s) as template." % (templateId))
+
+        butler = sensorRef.getButler()
+        template = butler.get(datasetType="calexp", dataId=templateId)
+        if self.config.doAddCalexpBackground:
+            templateBg = butler.get(datasetType="calexpBackground", dataId=templateId)
+            mi = template.getMaskedImage()
+            mi += templateBg.getImage()
+
+        if not template.hasPsf():
+            raise pipeBase.TaskError("Template has no psf")
+
+        templateSources = butler.get(datasetType="src", dataId=templateId)
+        return pipeBase.Struct(exposure = template,
+                               sources = templateSources)
