c2b8e11f37f0d5155e3a77967b3f6956354a8583
David Reiss <reiss@uw.edu>
2016-03-09 18:06:36 -0800

    actually add tests and make sure they pass

--- tests/testDipoleFitter.py
+++ tests/testDipoleFitter.py
@@ -19,9 +19,12 @@ import lsst.meas.base as measBase
 # import lsst.pex.config
 #import lsst.meas.deblender
 from lsst.meas.deblender import SourceDeblendTask
+import lsst.utils.tests
 
 ## First, test the algorithm itself (fitDipole_new()):
-class DipoleFitAlgorithmTest(unittest.TestCase):
+## Create a simulated diffim (with dipoles) and a linear background gradient in the pre-sub images
+##   then compare the input fluxes/centroids with the fitted results.
+class DipoleFitAlgorithmTest(lsst.utils.tests.TestCase):
     """ A test case for dipole fit algorithm"""
     def setUp(self):
         np.random.seed(666)
@@ -33,11 +36,12 @@ class DipoleFitAlgorithmTest(unittest.TestCase):
         self.flux = np.array([2500., 2345.])  ## fluxes of pos/neg lobes
         self.gradientParams = (10., 3., 5.)
 
+        offsets = np.array([-2., 2.])
         self.dipole, (self.posImage, self.posCatalog), (self.negImage, self.negCatalog) = \
-            dft.makeDipoleImage_lsst(xcenPos=self.xc + np.array([-2., +2.]),
-                                     ycenPos=self.yc + np.array([-2., +2.]),
-                                     xcenNeg=self.xc - np.array([-2., +2.]),
-                                     ycenNeg=self.yc - np.array([-2., +2.]),
+            dft.makeDipoleImage_lsst(xcenPos=self.xc + offsets,
+                                     ycenPos=self.yc + offsets,
+                                     xcenNeg=self.xc - offsets,
+                                     ycenNeg=self.yc - offsets,
                                      flux=self.flux, fluxNeg=self.flux,
                                      gradientParams=self.gradientParams)
 
@@ -45,7 +49,7 @@ class DipoleFitAlgorithmTest(unittest.TestCase):
                                                self.negImage, self.negCatalog)
 
     def tearDown(self):
-        pass
+        del self.dipole, self.posImage, self.negImage, self.catalog, self.posCatalog, self.negCatalog
 
     def testDipoleFitter(self):
         for s in self.catalog:
@@ -54,14 +58,20 @@ class DipoleFitAlgorithmTest(unittest.TestCase):
             for pk in fp.getPeaks():
                 print 'FOOTPRINT CENTER:', pk.getIy(), pk.getIx(), pk.getPeakValue()
 
-        for s in self.catalog:
+        offsets = np.array([-2., 2.])
+        for i,s in enumerate(self.catalog):
             result = dft.fitDipole_new(self.dipole, s, self.posImage, self.negImage,
                                    rel_weight=1., separateNegParams=False,
                                    verbose=False, display=False)
-            print result
+            self.assertClose((result.psfFitPosFlux + abs(result.psfFitNegFlux))/2., self.flux[i], rtol=0.02)
+            self.assertClose(result.psfFitPosCentroidX, self.xc[i] + offsets[i], rtol=0.01)
+            self.assertClose(result.psfFitPosCentroidY, self.yc[i] + offsets[i], rtol=0.01)
+            self.assertClose(result.psfFitNegCentroidX, self.xc[i] - offsets[i], rtol=0.01)
+            self.assertClose(result.psfFitNegCentroidY, self.yc[i] - offsets[i], rtol=0.01)
 
-## Second, test the task:
-class DipoleFitTaskTest(unittest.TestCase):
+## Second, test the task in the same way as the algorithm:
+## Also test that it correctly classifies the dipoles.
+class DipoleFitTaskTest(lsst.utils.tests.TestCase):
     """ A test case for dipole fit task"""
     def setUp(self):
         np.random.seed(666)
@@ -85,58 +95,35 @@ class DipoleFitTaskTest(unittest.TestCase):
                                                self.negImage, self.negCatalog)
 
     def tearDown(self):
-        pass
+        del self.dipole, self.posImage, self.negImage, self.catalog, self.posCatalog, self.negCatalog
 
     def testDipoleTask(self):
-        # Start with a minimal schema - only the fields all SourceCatalogs need
-        schema = afwTable.SourceTable.makeMinimalSchema()
+        ## Create the various tasks and schema -- avoid code reuse.
+        detectTask, deblendTask, schema = dft.detectDipoleSources(self.dipole, self.posImage,
+                                                                   self.posCatalog, self.negImage,
+                                                                   self.negCatalog, doMerge=False)
 
-        # Create and run a task for deblending (optional, but almost always a good idea). 
-        # Again, the task defines a few flag fields it will later fill. 
-        deblendTask = SourceDeblendTask(schema=schema)
-
-        # Customize the detection task a bit (optional)
-        detectConfig = measAlg.SourceDetectionConfig() 
-        detectConfig.returnOriginalFootprints = False # should be the default 
-        detectConfig.thresholdValue = 10 # only 10-sigma detections
-
-        ## code from imageDifference.py:
-        detectConfig.thresholdPolarity = "both"
-        detectConfig.thresholdValue = 5.5
-        detectConfig.reEstimateBackground = False
-        detectConfig.thresholdType = "pixel_stdev"
-
-        # Create the detection task. We pass the schema so the task can declare a few flag fields
-        detectTask = measAlg.SourceDetectionTask(config=detectConfig, schema=schema)
-
-        table = afwTable.SourceTable.make(schema)
-        detectResult = detectTask.run(table, self.dipole)
-        catalog = detectResult.sources
-        #results = detectTask.makeSourceCatalog(table, exposure, sigma=psfSigma)
-
-        deblendTask.run(self.dipole, catalog, psf=self.dipole.getPsf())
-
+        #measureConfig = dft.DipoleFitConfig()
         measureConfig = measBase.SingleFrameMeasurementConfig()
 
         # Modify the set of active plugins ('.names' behaves like a Python set)
-        #measureConfig.plugins.names.remove("base_GaussianCentroid")
-
-        # Enable some plugins - import the Python module first to make them available
-        #measureConfig.plugins.names |= ["modelfit_ShapeletPsfApprox", "modelfit_CModel"]
-
-        # Change which plugin's output we "bless" as the "Model Flux"
-        #measureConfig.slots.modelFlux = "modelfit_CModel"
+        measureConfig.plugins.names.remove("base_PsfFlux")
+        measureConfig.plugins.names.remove("base_GaussianCentroid")
+        # If I remove any of the following plugins, I get an 'base_GaussianFlux_flux' not found error
+        #measureConfig.plugins.names.remove("base_SdssCentroid")
+        #measureConfig.plugins.names.remove("base_GaussianFlux")
+        #measureConfig.plugins.names.remove("base_SdssShape")
 
         measureConfig.slots.modelFlux = "ip_diffim_DipoleFit"
 
-        #["base_CircularApertureFlux",
-    #                     "base_PixelFlags",
-    #                     "base_SkyCoord",
-    #                     "base_PsfFlux",
-    #                     #"ip_diffim_NaiveDipoleCentroid",
-    #                     #"ip_diffim_NaiveDipoleFlux",
-    #                     #"ip_diffim_PsfDipoleFlux"]
-    #                     DipoleFitTask._DefaultName]        
+        measureConfig.plugins.names |= ["base_CircularApertureFlux",
+                                        "base_PixelFlags",
+                                        "base_SkyCoord",
+                                        "base_PsfFlux",
+                                        "ip_diffim_NaiveDipoleCentroid",
+                                        "ip_diffim_NaiveDipoleFlux",
+                                        "ip_diffim_PsfDipoleFlux"]
+                                        #"ip_diffim_DipoleFit"]
 
         # Modify the internal configuration of one of the plugins
         #measureConfig.plugins["base_ClassificationExtendedness"].fluxRatio = 0.985
@@ -159,19 +146,41 @@ class DipoleFitTaskTest(unittest.TestCase):
 
         measureTask.run(sources, self.dipole, self.posImage, self.negImage)
 
-        for r1 in sources: #, r2 in zip(catalog, transformedCatalog):
-            print r1.extract("ip_diffim_DipoleFit*")
+        offsets = np.array([-2., 2.])
+        for i, r1 in enumerate(sources):
+            result = r1.extract("ip_diffim_DipoleFit*")
+            self.assertClose((result['ip_diffim_DipoleFit_pos_flux'] +
+                              abs(result['ip_diffim_DipoleFit_neg_flux']))/2., self.flux[i], rtol=0.02)
+            self.assertClose(result['ip_diffim_DipoleFit_pos_centroid_x'], self.xc[i] + offsets[i], rtol=0.01)
+            self.assertClose(result['ip_diffim_DipoleFit_pos_centroid_y'], self.yc[i] + offsets[i], rtol=0.01)
+            self.assertClose(result['ip_diffim_DipoleFit_neg_centroid_x'], self.xc[i] - offsets[i], rtol=0.01)
+            self.assertClose(result['ip_diffim_DipoleFit_neg_centroid_y'], self.yc[i] - offsets[i], rtol=0.01)
+            self.assertTrue(result['ip_diffim_DipoleFit_flag_classification'])
 
-        # Compare to input:
-        # ```
-        # # xc = npa([65.3, 24.2])
-        # # yc = npa([38.6, 78.5])
-        # # flux = npa([2500., 2345.])
-        # ```
-        # and output directly from `fitDipole_new()`:
-        # ```
-        # resultsOutput(psfFitPosCentroidX=26.132875038347073, psfFitPosCentroidY=80.489634132198631, psfFitNegCentroidX=22.080022018131551, psfFitNegCentroidY=76.444291337973098, psfFitPosFlux=2366.4792132019111, psfFitNegFlux=-2434.333049782319, psfFitCentroidX=24.106448528239312, psfFitCentroidY=78.466962735085872, psfFitOrientation=44.946864198037069, psfFitSignaltoNoise=57.14172173097851)
-        # ```
+            ## compare to the original ip_diffim_PsfDipoleFlux measurements
+            result2 = r1.extract("ip_diffim_PsfDipoleFlux*")
+            self.assertClose((result['ip_diffim_DipoleFit_pos_flux'] +
+                              abs(result['ip_diffim_DipoleFit_neg_flux']))/2.,
+                             (result2['ip_diffim_PsfDipoleFlux_pos_flux'] +
+                              abs(result2['ip_diffim_PsfDipoleFlux_neg_flux']))/2.,
+                             rtol=0.02)
+            self.assertClose(result['ip_diffim_DipoleFit_pos_centroid_x'],
+                             result2['ip_diffim_PsfDipoleFlux_pos_centroid_x'],
+                             rtol=0.01)
+            self.assertClose(result['ip_diffim_DipoleFit_pos_centroid_y'],
+                             result2['ip_diffim_PsfDipoleFlux_pos_centroid_y'],
+                             rtol=0.01)
+            self.assertClose(result['ip_diffim_DipoleFit_neg_centroid_x'],
+                             result2['ip_diffim_PsfDipoleFlux_neg_centroid_x'],
+                             rtol=0.01)
+            self.assertClose(result['ip_diffim_DipoleFit_neg_centroid_y'],
+                             result2['ip_diffim_PsfDipoleFlux_neg_centroid_y'],
+                             rtol=0.01)
+
+
+        #for r1 in sources:
+        #    print r1.extract("ip_diffim_PsfDipoleFlux*")
+        #    print r1.extract("base_CircularApertureFlux_25_*")
 
 def suite():
     """Returns a suite containing all the test cases in this module."""
@@ -181,7 +190,7 @@ def suite():
     suites = []
     suites += unittest.makeSuite(DipoleFitAlgorithmTest)
     suites += unittest.makeSuite(DipoleFitTaskTest)
-    #suites += unittest.makeSuite(tests.MemoryTestCase)
+    suites += unittest.makeSuite(tests.MemoryTestCase)
     return unittest.TestSuite(suites)
 
 def run(shouldExit = False):
