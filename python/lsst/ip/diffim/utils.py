db914fe9a229f0ff21ea2ba406e0e9f52faa260f
David Reiss <reiss@uw.edu>
2016-04-15 10:41:45 -0700

    update DipoleTestImage and address reviewer concerns
    makeStarImage() no longer returns a tuple of tuples

--- python/lsst/ip/diffim/utils.py
+++ python/lsst/ip/diffim/utils.py
@@ -732,10 +732,14 @@ def plotWhisker(results, newWcs):
     sp.set_title("WCS Residual")
     plt.show()
 
-# ################## UTILITY CLASS FOR DIPOLE TESTING ###
-
 
 class DipoleTestImage(object):
+    """Utility class for dipole measurement testing
+
+    Generate an image with simulated dipoles and noise; store the original "pre-subtraction" images
+    and catalogs as well.
+    Used to generate test data for DMTN-007.
+    """
 
     def __init__(self, w=101, h=101, xcenPos=[27.], ycenPos=[25.], xcenNeg=[23.], ycenNeg=[25.],
                  psfSigma=2., flux=[30000.], fluxNeg=None, noise=10., gradientParams=None):
@@ -752,45 +756,16 @@ class DipoleTestImage(object):
             self.fluxNeg = self.flux
         self.noise = noise
         self.gradientParams = gradientParams
-        self.diffim, (self.posImage, self.posCatalog), \
-            (self.negImage, self.negCatalog) = self._makeDipoleImage()
-
-    def _makeStarImage(self, xc=[15.3], yc=[18.6], flux=[2500], psfSigma=2., noise=10.0,
-                       gradientParams=None, schema=None):
-        """Generate an exposure and catalog with the given stellar source(s)"""
-
-        from lsst.meas.base.tests import TestDataset
-        bbox = afwGeom.Box2I(afwGeom.Point2I(0, 0), afwGeom.Point2I(self.w-1, self.h-1))
-        dataset = TestDataset(bbox, psfSigma=psfSigma, threshold=1.)
-
-        for i in xrange(len(xc)):
-            dataset.addSource(flux=flux[i], centroid=afwGeom.Point2D(xc[i], yc[i]))
-
-        if schema is None:
-            schema = TestDataset.makeMinimalSchema()
-        exposure, catalog = dataset.realize(noise=noise, schema=schema)
-
-        if gradientParams is not None:
-            y, x = np.mgrid[:self.w, :self.h]
-            gp = gradientParams
-            gradient = gp[0] + gp[1] * x + gp[2] * y
-            if len(gradientParams) > 3:  # it includes a set of 2nd-order polynomial params
-                gradient += gp[3] * x*y + gp[4] * x*x + gp[5] * y*y
-            imgArr = exposure.getMaskedImage().getArrays()[0]
-            imgArr += gradient
-
-        return exposure, catalog
+        self._makeDipoleImage()
 
     def _makeDipoleImage(self):
         """Generate an exposure and catalog with the given dipole source(s)"""
 
         posImage, posCatalog = self._makeStarImage(
-            xc=self.xcenPos, yc=self.ycenPos, flux=self.flux, psfSigma=self.psfSigma,
-            gradientParams=self.gradientParams, noise=self.noise)
+            xc=self.xcenPos, yc=self.ycenPos, flux=self.flux)
 
         negImage, negCatalog = self._makeStarImage(
-            xc=self.xcenNeg, yc=self.ycenNeg, flux=self.fluxNeg, psfSigma=self.psfSigma,
-            gradientParams=self.gradientParams, noise=self.noise)
+            xc=self.xcenNeg, yc=self.ycenNeg, flux=self.fluxNeg)
 
         dipole = posImage.clone()
         di = dipole.getMaskedImage()
@@ -805,7 +780,33 @@ class DipoleTestImage(object):
         dma = dm.getArray()
         # set the two custom mask planes to these new masks
         dma[:, :] = posDetectedBits*pos_det + negDetectedBits*neg_det
-        return dipole, (posImage, posCatalog), (negImage, negCatalog)
+        self.diffim, self.posImage, self.posCatalog, self.negImage, self.negCatalog \
+            = dipole, posImage, posCatalog, negImage, negCatalog
+
+    def _makeStarImage(self, xc=[15.3], yc=[18.6], flux=[2500], schema=None):
+        """Generate an exposure and catalog with the given stellar source(s)"""
+
+        from lsst.meas.base.tests import TestDataset
+        bbox = afwGeom.Box2I(afwGeom.Point2I(0, 0), afwGeom.Point2I(self.w-1, self.h-1))
+        dataset = TestDataset(bbox, psfSigma=self.psfSigma, threshold=1.)
+
+        for i in xrange(len(xc)):
+            dataset.addSource(flux=flux[i], centroid=afwGeom.Point2D(xc[i], yc[i]))
+
+        if schema is None:
+            schema = TestDataset.makeMinimalSchema()
+        exposure, catalog = dataset.realize(noise=self.noise, schema=schema)
+
+        if self.gradientParams is not None:
+            y, x = np.mgrid[:self.w, :self.h]
+            gp = self.gradientParams
+            gradient = gp[0] + gp[1] * x + gp[2] * y
+            if len(self.gradientParams) > 3:  # it includes a set of 2nd-order polynomial params
+                gradient += gp[3] * x*y + gp[4] * x*x + gp[5] * y*y
+            imgArr = exposure.getMaskedImage().getArrays()[0]
+            imgArr += gradient
+
+        return exposure, catalog
 
     def fitDipoleSource(self, source, **kwds):
         alg = dipoleFitTask.DipoleFitAlgorithm(self.diffim, self.posImage, self.negImage)
