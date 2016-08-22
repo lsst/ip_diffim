5d5b92b1f82d2d3d2f4773b6ebb7db39c798f638
fred3m <fredem@princeton.edu>
2016-08-22 16:59:31 -0400

    Update testKernelPca boilerplate

--- tests/testKernelPca.py
+++ tests/testKernelPca.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import unittest
 
-import lsst.utils.tests as tests
+import lsst.utils.tests
 import lsst.afw.image as afwImage
 import lsst.afw.math as afwMath
 import lsst.afw.geom as afwGeom
@@ -12,7 +12,7 @@ import lsst.pex.config as pexConfig
 
 pexLog.Trace_setVerbosity('lsst.ip.diffim', 5)
 
-class DiffimTestCases(unittest.TestCase):
+class DiffimTestCases(lsst.utils.tests.TestCase):
     
     def setUp(self):
         self.config    = ipDiffim.ImagePsfMatchTask.ConfigClass()
@@ -240,18 +240,12 @@ class DiffimTestCases(unittest.TestCase):
         
 #####
         
-def suite():
-    """Returns a suite containing all the test cases in this module."""
-    tests.init()
+class TestMemory(lsst.utils.tests.MemoryTestCase):
+    pass
 
-    suites = []
-    suites += unittest.makeSuite(DiffimTestCases)
-    suites += unittest.makeSuite(tests.MemoryTestCase)
-    return unittest.TestSuite(suites)
-
-def run(doExit=False):
-    """Run the tests"""
-    tests.run(suite(), doExit)
+def setup_module(module):
+    lsst.utils.tests.init()
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
