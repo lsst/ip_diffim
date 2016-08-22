423b510c391b0e1a78c05342d2cac779971cd8ea
fred3m <fredem@princeton.edu>
2016-08-22 16:47:01 -0400

    Update testIpDiffimConfig boilerplate

--- tests/testIpDiffimConfig.py
+++ tests/testIpDiffimConfig.py
@@ -24,10 +24,10 @@
 
 import unittest
 
-import lsst.utils.tests as tests
+import lsst.utils.tests
 import lsst.ip.diffim as ipDiffim
 
-class DiffimTestCases(unittest.TestCase):
+class DiffimTestCases(lsst.utils.tests.TestCase):
     def setUp(self):
         pass
 
@@ -80,19 +80,12 @@ class DiffimTestCases(unittest.TestCase):
 
 #####
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
-    tests.init()
+class TestMemory(lsst.utils.tests.MemoryTestCase):
+    pass
 
-    suites = []
-    suites += unittest.makeSuite(DiffimTestCases)
-    suites += unittest.makeSuite(tests.MemoryTestCase)
-    return unittest.TestSuite(suites)
-
-def run(doExit=False):
-    """Run the tests"""
-    tests.run(suite(), doExit)
+def setup_module(module):
+    lsst.utils.tests.init()
 
 if __name__ == "__main__":
-    run(True)
-
+    lsst.utils.tests.init()
+    unittest.main()
