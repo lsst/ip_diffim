8fcec81f17913b512cd807ea67e046b7e1896f24
fred3m <fredem@princeton.edu>
2016-08-22 13:11:36 -0400

    Update testImagePsfMatch boilerplate

--- tests/testImagePsfMatch.py
+++ tests/testImagePsfMatch.py
@@ -23,7 +23,7 @@
 #
 
 import unittest
-import lsst.utils.tests as tests
+import lsst.utils.tests
 import lsst.afw.image as afwImage
 import lsst.afw.math as afwMath
 import lsst.ip.diffim as ipDiffim
@@ -240,18 +240,12 @@ class PsfMatchTestCases(unittest.TestCase):
         del self.configDFr
         del self.psf
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
-    tests.init()
+class TestMemory(lsst.utils.tests.MemoryTestCase):
+    pass
 
-    suites = []
-    suites += unittest.makeSuite(PsfMatchTestCases)
-    suites += unittest.makeSuite(tests.MemoryTestCase)
-    return unittest.TestSuite(suites)
-
-def run(doExit=False):
-    """Run the tests"""
-    tests.run(suite(), doExit)
+def setup_module(module):
+    lsst.utils.tests.init()
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
