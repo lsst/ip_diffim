dcdbe44e4e7e7ee6869484932156e0692a5fcc23
fred3m <fredem@princeton.edu>
2016-08-22 16:52:48 -0400

    Update testKernelConadidateAndSolution boilerplate

--- tests/testKernelCandidateAndSolution.py
+++ tests/testKernelCandidateAndSolution.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import unittest
-import lsst.utils.tests as tests
+import lsst.utils.tests
 
 import lsst.utils
 import lsst.afw.geom as afwGeom
@@ -16,7 +16,7 @@ import lsst.afw.table as afwTable
 
 pexLog.Trace_setVerbosity('lsst.ip.diffim', 5)
 
-class DiffimTestCases(unittest.TestCase):
+class DiffimTestCases(lsst.utils.tests.TestCase):
 
     def setUp(self):
         schema = afwTable.SourceTable.makeMinimalSchema()
@@ -539,18 +539,12 @@ class DiffimTestCases(unittest.TestCase):
 
 #####
 
-def suite():
-    """Returns a suite containing all the test cases in this module."""
-    tests.init()
+class TestMemory(lsst.utils.tests.MemoryTestCase):
+    pass
 
-    suites = []
-    suites += unittest.makeSuite(DiffimTestCases)
-    suites += unittest.makeSuite(tests.MemoryTestCase)
-    return unittest.TestSuite(suites)
-
-def run(doExit=False):
-    """Run the tests"""
-    tests.run(suite(), doExit)
+def setup_module(module):
+    lsst.utils.tests.init()
 
 if __name__ == "__main__":
-    run(True)
+    lsst.utils.tests.init()
+    unittest.main()
