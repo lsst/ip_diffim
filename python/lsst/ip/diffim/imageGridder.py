ef564601245728c7ff8d4354f3837904fc9a79fd
David Reiss <reiss@uw.edu>
2017-02-15 17:05:02 -0800

    ImageGridder is now working

--- python/lsst/ip/diffim/imageGridder.py
+++ python/lsst/ip/diffim/imageGridder.py
@@ -171,7 +171,7 @@ class ImageGridderTask(pipeBase.Task):
         self.statsControl.setAndMask(afwImage.MaskU.getPlaneBitMask(self.config.ignoreMaskPlanes))
 
     @pipeBase.timeMethod
-    def run(self, exposure):
+    def run(self, exposure, returnPatches=False, **kwargs):
         """! Perform an operation on the given exposure.
 
         Break the exposure into sub-images on a grid (parameters given by `ImageGridderConfig`)
@@ -184,19 +184,53 @@ class ImageGridderTask(pipeBase.Task):
         """
         self.log.info("Processing.")
 
-        grid = self._generateGrid(exposure.getBBox())
+        boxes0, boxes1 = self._generateGrid(exposure)
+        if len(boxes0) != len(boxes1):
+            raise Exception('Uh oh!')   # TBD: define a specific exception to raise
 
-    def runSubImage(self, subImage, exposure):
+        mi = exposure.getMaskedImage()
+        patches = []
+        for i in range(len(boxes0)):
+            self.log.info("Processing on box: %s" % str(boxes0[i]))
+            subImage = afwImage.MaskedImageF(mi, boxes0[i]).clone()
+            expandedSubImage = afwImage.MaskedImageF(mi, boxes1[i]).clone()
+            result = self.runSubImage(subImage, expandedSubImage, exposure.getBBox(), **kwargs)
+            patches.append(result)
+
+        if returnPatches:
+            return patches
+
+        newMI = self._reduceImage(patches, exposure, **kwargs)
+        return newMI
+
+    @pipeBase.timeMethod
+    def runSubImage(self, subImage, expandedSubImage, fullBBox, **kwargs):
         """! Perform an operation on the given subImage.
 
         Return an image or exposure of the same dimensions as `subImage`.
+        Can use `expandedSubImage`, an expanded region of the original exposure,
+        to perform computations.
 
         @param[in] subImage the sub-image of `exposure` upon which to operate
-        @param[in] exposure the full exposure from which `subImage` is a sub-image
+        @param[in] expandedSubImage the expanded sub-image of `exposure` upon which to operate
+        @param[in] fullBBox the bounding box of the original exposure
         @return a `afw.Image` or `afw.Exposure`
         """
-        self.log.info("Processing on box: %s" % str(subImage.getBBox()))
-        pass
+        #subImage.getImage().getArray()[:, :] = 100.
+        img = subImage.getImage()
+        img += 100.
+        return subImage
+
+    def _reduceImage(self, patches, exposure, **kwargs):
+        newExp = afwImage.ExposureF(exposure).clone()
+        newMI = newExp.getMaskedImage()
+        ## TEST (make sure we are actually setting pixels in the new image):
+        newMI.getImage().getArray()[:, :] = 100.
+        ## END TEST
+        for patch in patches:
+            subim = afwImage.MaskedImageF(newMI, patch.getBBox())
+            subim.getImage()[:, :] = patch.getImage()[:, :]
+        return newExp
 
     def _generateGrid(self, exposure):
         """! Generate two lists of bounding boxes that evenly grid `exposure`
@@ -206,7 +240,7 @@ class ImageGridderTask(pipeBase.Task):
         Then the bounding boxes will be expanded by borderSizeX/Y. The expanded bounding
         boxes will be adjusted to ensure that they intersect the exposure's bounding box.
         The resulting lists of bounding boxes and corresponding expanded bounding boxes will
-        be returned.
+        be returned, and also set to `self.boxes0`, `self.boxes1`.
 
         @param[in] exposure an `afwImage.Exposure` whose full bounding box is to be evenly gridded.
         @return tuple containing two lists of `afwGeom.BoundingBox`es
@@ -232,10 +266,18 @@ class ImageGridderTask(pipeBase.Task):
             borderSizeX = rescaleValue(borderSizeX)
             borderSizeY = rescaleValue(borderSizeY)
 
-
         bbox = exposure.getBBox()
-
         nGridX = bbox.getWidth() // gridStepX
+        if rejiggerGridOption:
+            nGridX = bbox.getWidth() / gridStepX
+            # Readjust gridStepX so that it fits perfectly in the image.
+            gridStepX = float(bbox.getWidth() - gridSizeX) / float(nGridX)
+
+        nGridY = bbox.getWidth() // gridStepY
+        if rejiggerGridOption:
+            nGridY = bbox.getWidth() / gridStepY
+            # Readjust gridStepY so that it fits perfectly in the image.
+            gridStepY = float(bbox.getHeight() - gridSizeY) / float(nGridY)
 
         # first "main" box at 0,0
         bbox0 = afwGeom.Box2I(afwGeom.Point2I(bbox.getBegin()), afwGeom.Extent2I(gridSizeX, gridSizeY))
@@ -243,7 +285,11 @@ class ImageGridderTask(pipeBase.Task):
         bbox1 = afwGeom.Box2I(bbox0)
         bbox1.grow(afwGeom.Extent2I(borderSizeX, borderSizeY))
 
+        # Offset the "main" (bbox0) and "expanded" (bbox1) bboxes by xoff, yoff. Clip them by the
+        # exposure's bbox.
         def offsetAndClipBoxes(bbox0, bbox1, xoff, yoff, bbox):
+            xoff = int(np.floor(xoff))
+            yoff = int(np.floor(yoff))
             bb0 = afwGeom.Box2I(bbox0)
             bb0.shift(afwGeom.Extent2I(xoff, yoff))
             bb0.clip(bbox)
@@ -252,6 +298,53 @@ class ImageGridderTask(pipeBase.Task):
             bb1.clip(bbox)
             return bb0, bb1
 
+        boxes0 = []
+        boxes1 = []
+        xoff = 0
+        while(xoff <= bbox.getWidth()):
+            yoff = 0
+            while(yoff <= bbox.getHeight()):
+                bb0, bb1 = offsetAndClipBoxes(bbox0, bbox1, xoff, yoff, bbox)
+                boxes0.append(bb0)
+                boxes1.append(bb1)
+                yoff += gridStepY
+            xoff += gridStepX
+
+        return boxes0, boxes1
+
+    def _plotBoxGrid(self, boxes, bbox, **kwargs):
+        """! Plot a grid of boxes using matplotlib.
+
+        @param[in] boxes a list of `afwGeom.BoundginBox`es
+        @param[in] bbox an overall bounding box
+        """
+        import matplotlib.pyplot as plt
+
+        def plotBox(box):
+            corners = np.array([np.array([pt.getX(), pt.getY()]) for pt in box.getCorners()])
+            corners = np.vstack([corners, corners[0, :]])
+            plt.plot(corners[:, 0], corners[:, 1], **kwargs)
+
+        for b in boxes:
+            plotBox(b)
+        plt.xlim(bbox.getBeginX(), bbox.getEndX())
+        plt.ylim(bbox.getBeginY(), bbox.getEndY())
+
+    def _plotBoxes(self, exposure):
+        """! Plot both grids of boxes using matplotlib.
+
+        `self.boxes0` and `self.boxes1` must have been set.
+        @param[in] bbox an overall bounding box
+        """
+        import matplotlib.pyplot as plt
+
+        bbox = exposure.getBBox()
+        boxes0, boxes1 = self._generateGrid(exposure)
+        self._plotBoxGrid(boxes0[::3], bbox, ls='--')
+        # reset the color cycle -- see
+        # http://stackoverflow.com/questions/24193174/reset-color-cycle-in-matplotlib
+        plt.gca().set_prop_cycle(None)
+        self._plotBoxGrid(boxes1[::3], bbox, ls=':')
 
     def _computeVarianceMean(self, subImage):
         """! Utility function: compute mean of variance plane of subimage
