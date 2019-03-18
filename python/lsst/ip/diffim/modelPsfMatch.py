607217e16fa3ea46d2883acbd0415b892ea1e725
John Parejko <parejkoj@uw.edu>
2019-03-18 14:39:09 -0700

    Replace Calib->PhotoCalib

--- python/lsst/ip/diffim/modelPsfMatch.py
+++ python/lsst/ip/diffim/modelPsfMatch.py
@@ -313,7 +313,7 @@ class ModelPsfMatchTask(PsfMatchTask):
         -------
         result : `struct`
             - ``psfMatchedExposure`` : the Psf-matched Exposure.
-                This has the same parent bbox, Wcs, Calib and
+                This has the same parent bbox, Wcs, PhotoCalib and
                 Filter as the input Exposure but no Psf.
                 In theory the Psf should equal referencePsfModel but
                 the match is likely not exact.
@@ -353,7 +353,7 @@ class ModelPsfMatchTask(PsfMatchTask):
         self.log.info("Psf-match science exposure to reference")
         psfMatchedExposure = afwImage.ExposureF(exposure.getBBox(), exposure.getWcs())
         psfMatchedExposure.setFilter(exposure.getFilter())
-        psfMatchedExposure.setCalib(exposure.getCalib())
+        psfMatchedExposure.setPhotoCalib(exposure.getPhotoCalib())
         psfMatchedExposure.getInfo().setVisitInfo(exposure.getInfo().getVisitInfo())
         psfMatchedExposure.setPsf(referencePsfModel)
         psfMatchedMaskedImage = psfMatchedExposure.getMaskedImage()
