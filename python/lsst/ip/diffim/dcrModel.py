Fix parallactic angle calculation to pass new unit tests

python/lsst/ip/diffim/dcrModel.py
@@ -708,7 +708,7 @@ def calculateImageParallacticAngle(visitInfo, wcs):
         rotAngle = (cdAngle + parAngle)*radians
     else:
         cdAngle = (np.arctan2(cd[0, 1], -cd[0, 0]) + np.arctan2(cd[1, 0], cd[1, 1]))/2.
-        rotAngle = -(cdAngle + parAngle)*radians
+        rotAngle = (cdAngle - parAngle)*radians
     return rotAngle
 
 