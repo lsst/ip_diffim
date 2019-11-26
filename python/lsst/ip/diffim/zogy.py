1788c3412fdeb7c98eb252b5f30ac8dfa3249ea3
Michael Wood-Vasey <wmwv@pitt.edu>
2019-11-26 07:30:31 -0800

    Add scaleByCalibration option to ZogyTask config.

--- python/lsst/ip/diffim/zogy.py
+++ python/lsst/ip/diffim/zogy.py
@@ -98,6 +98,12 @@ class ZogyConfig(pexConfig.Config):
         doc="Science flux scaling factor (Fn in ZOGY paper)"
     )
 
+    scaleByCalibration = pexConfig.Field(
+        dtype=bool,
+        default=True,
+        doc="Compute the flux normalization scaling based on the image calibration.  This overrides 'templateFluxScaling' and 'scienceFluxScaling'.",
+    )
+
     doTrimKernels = pexConfig.Field(
         dtype=bool,
         default=False,
@@ -290,8 +296,16 @@ class ZogyTask(pipeBase.Task):
             _subtractImageMean(self.template)
             _subtractImageMean(self.science)
 
+        # Define the normalization of each image from the config
         self.Fr = self.config.templateFluxScaling  # default is 1
         self.Fn = self.config.scienceFluxScaling  # default is 1
+        # If 'scaleByCalibration' is True then these norms are overwritten
+        if self.config.scaleByCalibration:
+            calib_template = self.template.getPhotoCalib()
+            calib_exposure = self.science.getPhotoCalib()
+            self.Fr = calib_template.getCalibrationMean()
+            self.Fn = calib_exposure.getCalibrationMean()
+
         self.padSize = self.config.padSize  # default is 7
 
     def _computeVarianceMean(self, exposure):
