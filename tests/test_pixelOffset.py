8f1026f41404a511d1256d695a3b18502da87399
Shu Liu <liushu2011@outlook.com>
2020-03-02 19:21:36 -0800

    add docstrings to test_pixelOffset.py

--- tests/test_pixelOffset.py
+++ tests/test_pixelOffset.py
@@ -10,20 +10,23 @@ from lsst.ip.diffim.zogy import ZogyTask, ZogyConfig
 class PixelOffsetTest(unittest.TestCase):
     """A test case for the pixel offset.
     """
+    
     @staticmethod
-    def _makeImage(w, h, psfsig, set_peak=False):
+    def _makeImage(w, h, psfsize, psfsig, set_peak=False):
+        """Make fake images for testing. If set_peak=True, the
+        image would have a peak at [50][50].
+        """
         # Make image
         image = afwImage.MaskedImageF(w, h)
         image.set(0)
         array = image.getImage().getArray()
         if set_peak == True:
             # set the peak value
-            array[50][50] = 1000
+            array[300][300] = 1000
         var = image.getVariance()
         var.set(1.0)
         # Make PSF
-        psfSize = 17
-        psf = measAlg.DoubleGaussianPsf(psfSize, psfSize, psfsig, psfsig, 0.)
+        psf = measAlg.DoubleGaussianPsf(psfsize, psfsize, psfsig, psfsig, 0.)
         # Make exposure
         exp = afwImage.makeExposure(image)
         exp.setPsf(psf)
@@ -31,17 +34,23 @@ class PixelOffsetTest(unittest.TestCase):
     
     @staticmethod
     def _find_max(data):
+        """ Find the maximum position of the data.
+        """
         return np.unravel_index(np.argmax(data), data.shape)
 
     def _setUpImages(self):
-        self.imrex = PixelOffsetTest._makeImage(100, 100, 1, set_peak=False)
-        self.imnex = PixelOffsetTest._makeImage(100, 100, 2, set_peak=True)
+        """ Setup test images.
+        """
+        self.imrex = PixelOffsetTest._makeImage(2000, 4000, 17, 1, set_peak=False)
+        self.imnex = PixelOffsetTest._makeImage(2000, 4000, 17, 2, set_peak=True)
         
     def testPixelOffset(self):
+        """ Test whether the peak position is at [300][300].
+        """
         self._setUpImages()
         config = ZogyConfig()
         task = ZogyTask(templateExposure=self.imrex, scienceExposure=self.imnex, config=config)
         D_F = task.computeDiffim(inImageSpace=False)
         max_loc = PixelOffsetTest._find_max(D_F.D.image.array)
     
-        self.assertEqual(max_loc, (50, 50))
+        self.assertEqual(max_loc, (300, 300))
