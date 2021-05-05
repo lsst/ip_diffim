9913fdb0cf49b2efada9e77e96a72001d1be77db
Jonathan Sick <jsick@lsst.org>
2021-05-05 13:12:53 -0400

    Conform doc/conf.py to template

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,7 @@
 """Sphinx configuration file for an LSST stack package.
-
-This configuration only affects single-package Sphinx documenation builds.
+This configuration only affects single-package Sphinx documentation builds.
+For more information, see:
+https://developer.lsst.io/stack/building-single-package-docs.html
 """
 
 from documenteer.conf.pipelinespkg import *
