c7564498c48eccad4d629df4365d2294d4a1b62c
Nima <nimaseda@uw.edu>
2023-02-14 17:25:58 -0800

    Added subtractImagesTransiNet to __init__

--- python/lsst/ip/diffim/__init__.py
+++ python/lsst/ip/diffim/__init__.py
@@ -44,6 +44,7 @@ from .detectAndMeasure import *
 from .imageDecorrelation import *
 from .imageMapReduce import *
 from .subtractImages import *
+from .subtractImagesTransiNet import *
 from .zogy import *
 from .version import *
 
