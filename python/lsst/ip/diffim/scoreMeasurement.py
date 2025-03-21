06b24c865b89bd17e5bb4389b17930f40b35c73b
John Parejko <parejkoj@uw.edu>
2025-03-21 13:58:22 -0700

    In progress score image measurement

---
+++ python/lsst/ip/diffim/scoreMeasurement.py
@@ -0,0 +1,180 @@
+# This file is part of ip_diffim.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = ["SingleFrameScoreMeasurementTask"]
+
+import numpy as np
+# import scipy.signal
+
+import lsst.afw.image
+import lsst.geom
+import lsst.meas.base
+
+
+class SingleFrameScoreMeasurementConfig(lsst.meas.base.BaseMeasurementConfig):
+    """Config class for single frame measurement driver task.
+    """
+
+    plugins = lsst.meas.base.SingleFramePlugin.registry.makeField(
+        multi=True,
+        default=[],
+        doc="Plugins to be run and their configuration"
+    )
+    undeblended = lsst.meas.base.SingleFramePlugin.registry.makeField(
+        multi=True,
+        default=[],
+        doc="Plugins to run on undeblended image"
+    )
+
+import lsst.afw.display
+display = lsst.afw.display.Display()
+display.frame = 3
+
+
+class SingleFrameScoreMeasurementTask(lsst.meas.base.SingleFrameMeasurementTask):
+    """Run measurement plugins on a likelihood/score image, like that produced
+    by `~lsst.ip.diffim.AlardLuptonPreconvolveSubtractTask`.
+    """
+    # TODO: need to create an appropriate "cannot deconvolve" flag
+    # TODO: will have to override run and runPlugins, since I need to
+    # pass other arguments.
+
+    # For FFT deconvolve, the kernel and cutout must be the same size.
+    # Can we get a gaussian parameterization of the PSF instead of using
+    # a PSF image? If so, then calculate the image of the PSF within the
+    # cutout box, and use that.
+    # use np.fft for simplicity
+
+    def callMeasure(self, record, exposure, kernel, *args, **kwargs):
+        beginOrder = kwargs.pop("beginOrder", None)
+        endOrder = kwargs.pop("endOrder", None)
+
+        self.doMeasurement(self.plugins["base_SdssCentroid"], record, exposure=exposure,
+                           *args, **kwargs)
+
+        # TODO: either we have to increase the size of the kernel image box, or
+        # the size of the cutout, so that they match. Always use the largest size.
+        box = record.getFootprint().getBBox()
+        # padded = lsst.geom.Box2I(box)
+        # padded.grow(box.height//2)
+        dim = box.getDimensions()
+        dim.x += 1
+        dim.y += 1
+        box = lsst.geom.Box2I.makeCenteredBox(box.getCenter(), dim)
+        kernelBox = kernel.computeBBox(record.getCentroid())
+        # if (box.width < kernelBox.width) or (box.height < kernelBox.height):
+        #     raise RuntimeError("Can't handle this yet!")
+
+        # center = lsst.geom.Point2D(record.getCentroid())
+        # center.shift(lsst.geom.Extent2D(1, 1))
+        # localKernel = kernel.getLocalKernel(record.getCentroid())
+        kernelImage = lsst.afw.image.ImageD(box)
+        # import os; print(os.getpid()); import ipdb; ipdb.set_trace();
+        tempBox = lsst.geom.Box2I.makeCenteredBox(box.getCenter(), kernelBox.getDimensions())
+        # localKernel.computeImage(kernelImage[tempBox], doNormalize=True)
+        kernelImage[tempBox] = kernel.computeKernelImage(record.getCentroid())
+        # kernelImage[box.getCenter()] = 1
+        deconvolved = lsst.afw.image.ExposureD(box)
+        shape = deconvolved.image.array.shape
+        cutout = lsst.afw.image.ImageD(box)
+        cutout[box].array = exposure.image[box].array
+        # TODO: restore rfft2 once we've debugged things (will make slicing harder)
+        fftexp = np.fft.fft2(exposure.image[box].array, shape)
+        fftkernel = np.fft.fft2(np.roll(kernelImage.array, (0, 0)), shape)
+        quotient = np.zeros_like(fftkernel)
+        # TODO: How do we choose the best region to remove
+        slice = np.s_[12:17, 12:17]
+        quotient[slice] = np.fft.fftshift(fftexp / np.abs(fftkernel))[slice]
+        deconvolved.image.array = np.fft.ifft2(np.fft.fftshift(quotient),
+                                               deconvolved.image.array.shape).real
+        temp = lsst.afw.image.ImageD(box)
+        display.frame += 1
+        display.image(kernelImage, title="kernelImage")
+        display.frame += 1
+        display.image(deconvolved, title="deconvolved")
+        display.frame += 1
+        display.image(cutout, title="exposure")
+        display.frame += 1
+        temp.array = np.fft.fftshift(np.array(np.abs(fftexp), dtype=np.float64))
+        display.image(temp, title="fftexp")
+        display.frame += 1
+        temp.array = np.fft.fftshift(np.abs(fftkernel))
+        display.image(temp, title="fftkernel")
+        display.frame += 1
+        temp.array = np.abs(np.quotient)
+        display.image(temp, title="fftexp/fftkernel")
+        import os; print(os.getpid()); import ipdb; ipdb.set_trace();
+        # display.frame = 1
+        # fftkernel = np.fft.fft2(np.roll(kernelImage.array, (0, 0))))
+        # deconvolved.image.array = np.fft.ifft2(fftexp/fftkernel),
+        #                                        deconvolved.image.array.shape).real
+        # display.image(deconvolved, title="no shift")
+        # display.frame += 1
+        # fftkernel = np.fft.fft2(np.roll(kernelImage.array, (1, 0))))
+        # deconvolved.image.array = np.fft.ifft2(fftexp/fftkernel),
+        #                                        deconvolved.image.array.shape).real
+        # display.image(deconvolved, title="x=1")
+        # display.frame += 1
+        # fftkernel = np.fft.fft2(np.roll(kernelImage.array, (-1, 0))))
+        # deconvolved.image.array = np.fft.ifft2(fftexp/fftkernel),
+        #                                        deconvolved.image.array.shape).real
+        # display.image(deconvolved, title="x=-1")
+        # display.frame += 1
+        # fftkernel = np.fft.fft2(np.roll(kernelImage.array, (0, 1))))
+        # deconvolved.image.array = np.fft.ifft2(fftexp/fftkernel),
+        #                                        deconvolved.image.array.shape).real
+        # display.image(deconvolved, title="y=1")
+        # display.frame += 1
+        # fftkernel = np.fft.fft2(np.roll(kernelImage.array, (0, -1))))
+        # deconvolved.image.array = np.fft.ifft2(fftexp/fftkernel),
+        #                                        deconvolved.image.array.shape).real
+        # display.image(deconvolved, title="y=-1")
+        # display.frame += 1
+        # fftkernel = np.fft.fft2(np.roll(kernelImage.array, (1, 1))))
+        # deconvolved.image.array = np.fft.ifft2(fftexp/fftkernel),
+        #                                        deconvolved.image.array.shape).real
+        # display.image(deconvolved, title="both=1")
+        # display.frame += 1
+        # fftkernel = np.fft.fft2(np.roll(kernelImage.array, (-1, -1))))
+        # deconvolved.image.array = np.fft.ifft2(fftexp/fftkernel),
+        #                                        deconvolved.image.array.shape).real
+        # display.image(deconvolved, title="both=-1")
+
+        # # display.frame = 1
+        # # display.image(fftexp, title="fftexp")
+        # # display.frame = 1
+        # # display.image(fftkernel, title="fftkernel")
+        # # display.frame = 1
+        # # display.image(fftexp/fftkernel, title="fftexp/fftkernel")
+
+        import os; print(os.getpid()); import ipdb; ipdb.set_trace();
+
+        popped = self.plugins.pop("base_SdssCentroid")
+        for plugin in self.plugins.iter():
+            if beginOrder is not None and plugin.getExecutionOrder() < beginOrder:
+                continue
+            if endOrder is not None and plugin.getExecutionOrder() >= endOrder:
+                break
+            self.doMeasurement(plugin, record, exposure=deconvolved,
+                               *args, **kwargs)
+
+        self.plugins["base_SdssCentroid"] = popped
+        return
---
+++ tests/test_singleFrameScoreMeasurement.py
@@ -0,0 +1,128 @@
+# This file is part of ip_diffim.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import unittest
+
+import lsst.afw.image
+import lsst.afw.math
+import lsst.afw.table
+import lsst.geom
+import lsst.ip.diffim
+from lsst.meas.base.pluginRegistry import register
+from lsst.meas.base.tests import TestDataset
+import lsst.utils.tests
+
+
+@register("base_scoreTest")
+class ScoreTestPlugin(lsst.meas.base.SingleFramePlugin):
+
+    def __init__(self, config, name, schema, metadata):
+        super().__init__(config, name, schema, metadata)
+        self.key = schema.addField(name + "_value", type="F", doc="test value")
+        # self.keyY = schema.addField(name + "_y", type="D", doc="peak centroid", units="pixel")
+
+    @classmethod
+    def getExecutionOrder(cls):
+        return cls.SHAPE_ORDER
+
+    def measure(self, record, exposure, kernel=None):
+        center = record.getCentroid()
+        flux = exposure.image[center]
+        record.set(self.key, flux)
+
+
+import lsst.afw.display
+
+
+class SingleFrameScoreMeasurementTest(lsst.utils.tests.TestCase):
+    """Test that SingleFrameScoreMeasurementTask properly deconvolves
+    the image.
+    """
+    def setUp(self):
+        bbox = lsst.geom.Box2I(lsst.geom.Point2I(5, 4), lsst.geom.Point2I(155, 125))
+        dataset = TestDataset(bbox, psfSigma=4.0, psfDim=27)
+        # two sources, separated by about twice our PSF image size.
+        dataset.addSource(1e4, lsst.geom.Point2D(50, 50))
+        dataset.addSource(1e5, lsst.geom.Point2D(80, 50))
+        self.exposure, self.catalog = dataset.realize(2.0, dataset.makeMinimalSchema())
+
+        convolutionControl = lsst.afw.math.ConvolutionControl()
+        # convolutionControl.setDoNormalize(False)
+        # convolutionControl.setDoCopyEdge(True)
+        image = lsst.afw.image.MaskedImageD(self.exposure.getBBox())
+        # import os; print(os.getpid()); import ipdb; ipdb.set_trace();
+        self.score = lsst.afw.math.convolve(image,
+                                            self.exposure.maskedImage,
+                                            self.exposure.psf.getLocalKernel(self.catalog[0].getCentroid()),
+                                            convolutionControl)
+        self.score = lsst.afw.image.ExposureD(self.exposure, deep=True)
+        self.score.maskedImage = image
+
+    def test_callMeasure(self):
+        schema = lsst.afw.table.SourceTable.makeMinimalSchema()
+
+        config = lsst.ip.diffim.SingleFrameScoreMeasurementTask.ConfigClass()
+        config.plugins = ["base_scoreTest", "base_SdssCentroid"]
+        config.slots.apFlux = None
+        config.slots.calibFlux = None
+        config.slots.gaussianFlux = None
+        config.slots.modelFlux = None
+        config.slots.psfFlux = None
+        config.slots.shape = None
+        config.slots.psfShape = None
+        task = lsst.ip.diffim.SingleFrameScoreMeasurementTask(schema=schema,
+                                                              config=config)
+        catalog = lsst.afw.table.SourceCatalog(schema)
+        catalog.addNew()
+        catalog[-1].setFootprint(self.catalog[0].getFootprint())
+        catalog.addNew()
+        catalog[-1].setFootprint(self.catalog[1].getFootprint())
+
+        # display = lsst.afw.display.Display()
+        # display.frame = 1
+        # display.image(self.exposure, title="exposure")
+        # display.frame = 2
+        # display.image(self.score, title="score")
+        # display.centroids(catalog, size=10, ctype="red", symbol="x")
+        # for x in catalog:
+        #     display.dot(x['id'], x.getX(), x.getY(), size=10, ctype="cyan")
+
+        for record in catalog:
+            task.callMeasure(record, self.score, kernel=self.exposure.psf)
+
+        for record in catalog:
+            with self.subTest(f"record {record['id']} at {record.getCentroid()}"):
+                self.assertFloatsAlmostEqual(record["base_scoreTest_value"],
+                                             self.exposure.image[record.getCentroid()])
+
+        # import ipdb; ipdb.set_trace();
+
+
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
+class MemoryTestCase(lsst.utils.tests.MemoryTestCase):
+    pass
+
+
+if __name__ == "__main__":
+    lsst.utils.tests.init()
+    unittest.main()
